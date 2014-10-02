078e16e40241ee4794e4df97a70633313a2faf67
Karl Broman <kbroman@gmail.com>
2014-10-01 22:26:57 -0400

    cosmetic changes

--- src/cross_bc.cpp
+++ src/cross_bc.cpp
@@ -5,32 +5,34 @@
 double BC::init(int true_gen, bool is_X_chr, bool is_female,
                 vector<int> cross_info)
 {
-    return(-log(0.5)); /* ln(0.5) */
+    return log(0.5);
 }
 
-double BC::emit(int obs_gen, int true_gen, double error_prob, 
+double BC::emit(int obs_gen, int true_gen, double error_prob,
                 bool is_X_chr, bool is_female,
                 vector<int> cross_info)
 {
     switch(obs_gen) {
-    case 0: return(0.0);
+    case 0: return(0.0); // missing value
     case 1: case 2:
-        if(obs_gen==true_gen) return(log(1.0-error_prob));
-        else return(log(error_prob));
+        if(obs_gen==true_gen) return log(1.0-error_prob);
+        else return log(error_prob);
     }
+
     Rcpp::exception("invalid obs_gen or true_gen");
-    return(0.0); /* shouldn't get here */
+    return NA_REAL; // can't get here
 }
 
 
-double BC::step(int gen_left, int gen_right, double rec_frac, 
+double BC::step(int gen_left, int gen_right, double rec_frac,
                 bool is_X_chr, bool is_female,
                 vector<int> cross_info)
 {
-    if(gen_left==gen_right) return(log(1.0-rec_frac));
-    else return(log(rec_frac));
+    if(gen_left==gen_right) return log(1.0-rec_frac);
+    else return log(rec_frac);
+
     Rcpp::exception("invalid genotypes");
-    return(log(-1.0)); /* shouldn't get here */
+    return NA_REAL; // can't get here
 }
 
 vector<int> BC::geno(bool is_X_chr, bool is_female,
--- src/cross_f2.cpp
+++ src/cross_f2.cpp
@@ -2,61 +2,61 @@
 #include <Rcpp.h>
 #include "cross.h"
 
-double F2::init(int true_gen, 
+double F2::init(int true_gen,
                 bool is_X_chr, bool is_female,
                 vector<int> cross_info)
 {
-    if(true_gen==2) return(-log(0.5)); /* ln(0.5) */
-    if(true_gen==1 || true_gen==3) return(-2.0*log(0.5)); /* ln(0.25) */
+    if(true_gen==2) return log(0.5);
+    if(true_gen==1 || true_gen==3) return log(0.25);
 
     Rcpp::exception("invalid genotype");
-    return(0.0); /* can't get here */
+    return NA_REAL; // can't get here
 }
 
-double F2::emit(int obs_gen, int true_gen, double error_prob, 
+double F2::emit(int obs_gen, int true_gen, double error_prob,
                 bool is_X_chr, bool is_female,
                 vector<int> cross_info)
 {
     switch(obs_gen) {
     case 0: return(0.0);
     case 1: case 2: case 3:
-        if(obs_gen==true_gen) return(log(1.0-error_prob));
-        else return(log(error_prob)-log(0.5));
+        if(obs_gen==true_gen) return log(1.0-error_prob);
+        else return log(error_prob*0.5);
     case 4: /* AA or AB (not BB) */
-        if(true_gen != 3) return(log(1.0-error_prob/2.0));
-        else return(log(error_prob));
+        if(true_gen != 3) return log(1.0-error_prob*0.5);
+        else return log(error_prob);
     case 5: /* AB or BB (not AA) */
-        if(true_gen != 1) return(log(1.0-error_prob/2.0));
-        else return(log(error_prob));
+        if(true_gen != 1) return log(1.0-error_prob*0.5);
+        else return log(error_prob);
     }
     Rcpp::exception("invalid obs_gen or true_gen");
-    return(0.0); /* shouldn't get here */
+    return NA_REAL; /* shouldn't get here */
 }
 
 
-double F2::step(int gen_left, int gen_right, double rec_frac, 
+double F2::step(int gen_left, int gen_right, double rec_frac,
                 bool is_X_chr, bool is_female,
                 vector<int> cross_info)
 {
     switch(gen_left) {
     case 1:
         switch(gen_right) {
-        case 1: return(2.0*log(1.0-rec_frac));
-        case 2: return(log(0.5)+log(1.0-rec_frac)+log(rec_frac));
-        case 3: return(2.0*log(rec_frac));
+        case 1: return 2.0*log(1.0-rec_frac);
+        case 2: return log(0.5)+log(1.0-rec_frac)+log(rec_frac);
+        case 3: return 2.0*log(rec_frac);
         }
     case 2:
         switch(gen_right) {
-        case 1: case 3: return(log(rec_frac)+log(1.0-rec_frac));
-        case 2: return(log((1.0-rec_frac)*(1.0-rec_frac)+rec_frac*rec_frac));
+        case 1: case 3: return log(rec_frac)+log(1.0-rec_frac);
+        case 2: return log((1.0-rec_frac)*(1.0-rec_frac)+rec_frac*rec_frac);
         }
     case 3:
         switch(gen_right) {
-        case 1: return(2.0*log(rec_frac));
-        case 2: return(log(0.5)+log(1.0-rec_frac)+log(rec_frac));
-        case 3: return(2.0*log(1.0-rec_frac));
+        case 1: return 2.0*log(rec_frac);
+        case 2: return log(0.5)+log(1.0-rec_frac)+log(rec_frac);
+        case 3: return 2.0*log(1.0-rec_frac);
         }
     }
     Rcpp::exception("invalid genotypes.");
-    return(log(-1.0)); /* shouldn't get here */
+    return NA_REAL; // can't get here
 }
