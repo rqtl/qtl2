4a709e1a2c350b69793f6eea25c77b6b27b503a1
Karl Broman <kbroman@gmail.com>
2014-10-02 08:11:04 -0400

    Fix up risib

--- src/cross_risib.cpp
+++ src/cross_risib.cpp
@@ -5,16 +5,26 @@
 #include <Rcpp.h>
 #include "cross.h"
 
-enum ogen {NA=0, A=1, B=2};
-enum tgen {AA=1, BB=2};
+enum gen {NA=0, AA=1, BB=2};
+
+bool RIsib::check_geno(int gen, bool is_observed_value,
+                       bool is_X_chr, bool ignored, vector<int> cross_info)
+{
+    if(is_observed_value && gen==NA) return true;
+
+    if(gen != AA && gen != BB)
+        Rcpp::exception("genotype not allowed.");
+
+    return true;
+}
+
 
 double RIsib::init(int true_gen,
                    bool is_X_chr, bool ignored, vector<int> cross_info)
 {
-    const bool forward_direction = (cross_info[0] == 0); // AA female x BB male
+    check_geno(true_gen, false, is_X_chr, ignored, cross_info);
 
-    if(true_gen != AA && true_gen != BB)
-        Rcpp::exception("genotype not allowed.");
+    const bool forward_direction = (cross_info[0] == 0); // AA female x BB male
 
     if(is_X_chr) {
         if(forward_direction) {
@@ -30,32 +40,26 @@ double RIsib::init(int true_gen,
         return -log(2.0);
     }
 
-    return NA_REAL;
+    return NA_REAL; // can't get here
 }
 
 double RIsib::emit(int obs_gen, int true_gen, double error_prob,
                    bool is_X_chr, bool ignored, vector<int> cross_info)
 {
-    if(obs_gen==NA) return 0.0; // missing
+    check_geno(obs_gen, true, is_X_chr, ignored, cross_info);
+    check_geno(true_gen, false, is_X_chr, ignored, cross_info);
 
-    if(obs_gen!=A && obs_gen!=B)
-        Rcpp::exception("Incorrect value for obs_gen");
-    if(true_gen!=AA && true_gen!=BB)
-        Rcpp::exception("Incorrect value for true_gen");
+    if(obs_gen==NA) return 0.0; // missing
 
     if(obs_gen == true_gen) return log(1.0 - error_prob);
     else return log(error_prob);
-
-    return NA_REAL;
 }
 
 double RIsib::step(int gen_left, int gen_right, double rec_frac,
                    bool is_X_chr, bool ignored, vector<int> cross_info)
 {
-    if(gen_left!=AA && gen_left!=BB)
-        Rcpp::exception("Incorrect value for gen_left");
-    if(gen_right!=AA && gen_right!=BB)
-        Rcpp::exception("Incorrect value for gen_right");
+    check_geno(gen_left, false, is_X_chr, ignored, cross_info);
+    check_geno(gen_right, false, is_X_chr, ignored, cross_info);
 
     if(is_X_chr)  {
         const bool forward_direction = (cross_info[0] == 0); // AA female x BB male
@@ -79,14 +83,14 @@ double RIsib::step(int gen_left, int gen_right, double rec_frac,
             if(gen_left == AA) {
                 if(gen_right == AA)
                     return -log(1.0 + 4.0*rec_frac);
-                if(gen_right == BB) // A -> B
+                if(gen_right == BB)
                     return log(4.0*rec_frac) - log(1.0 + 4.0*rec_frac);
             }
 
             if(gen_left == BB) {
                 if(gen_right == BB)
                     return log(1.0 + 2.0*rec_frac) - log(1.0 + 4.0*rec_frac);
-                if(gen_right == BB)
+                if(gen_right == AA)
                     return log(2.0*rec_frac) - log(1.0 + 4.0*rec_frac);
             }
         }
@@ -101,7 +105,7 @@ double RIsib::step(int gen_left, int gen_right, double rec_frac,
     return NA_REAL; // can't get here
 }
 
-vector<int> RIsib::geno(bool is_X_chr, bool ignored, vector<int> cross_info)
+vector<int> RIsib::allgeno(bool is_X_chr)
 {
     int vals[] = {AA,BB};
     vector<int> result(vals, vals+2);
@@ -111,10 +115,8 @@ vector<int> RIsib::geno(bool is_X_chr, bool ignored, vector<int> cross_info)
 double RIsib::nrec(int gen_left, int gen_right,
                    bool is_X_chr, bool ignored, vector<int> cross_info)
 {
-    if(gen_left!=AA && gen_left!=BB)
-        Rcpp::exception("Incorrect value for gen_left");
-    if(gen_right!=AA && gen_right!=BB)
-        Rcpp::exception("Incorrect value for gen_right");
+    check_geno(gen_left, false, is_X_chr, ignored, cross_info);
+    check_geno(gen_right, false, is_X_chr, ignored, cross_info);
 
     if(gen_left == gen_right) return 0.0;
     else return 1.0;
--- src/cross_risib.h
+++ src/cross_risib.h
@@ -13,15 +13,20 @@ class RIsib : public Cross
     };
     ~RIsib(){};
 
+    bool check_geno(int gen, bool is_observed_value,
+                    bool is_X_chr, bool is_female, vector<int> cross_info);
+
     double init(int true_gen,
                 bool is_X_chr, bool is_female, vector<int> cross_info);
     double emit(int obs_gen, int true_gen, double error_prob,
                 bool is_X_chr, bool is_female, vector<int> cross_info);
     double step(int gen_left, int gen_right, double rec_frac,
                 bool is_X_chr, bool is_female, vector<int> cross_info);
-    vector<int> geno(bool is_X_chr, bool is_female, vector <int>cross_info);
+
+    vector<int> allgeno(bool is_X_chr);
+
     double nrec(int gen_left, int gen_right,
-                bool ignored1, bool ignored2, vector<int> ignored3);
+                bool is_X_chr, bool is_female, vector<int> cross_info);
 };
 
 #endif // CROSS_RISIB_H
