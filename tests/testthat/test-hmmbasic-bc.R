79ee92c17484b321f2ac94331e0b15c68f7a5f0c
Karl Broman <kbroman@gmail.com>
2014-10-02 21:20:43 -0400

    Add tests of basic HMM functions for backcross

---
+++ tests/testthat/test-hmmbasic-bc.R
@@ -0,0 +1,281 @@
+
+context("basic HMM functions")
+
+test_that("backcross check_geno works", {
+
+    # autosome
+    expect_true(test_check_geno("bc", 0, TRUE, FALSE, FALSE, numeric(0), FALSE))
+    expect_true(test_check_geno("bc", 1, TRUE, FALSE, FALSE, numeric(0), FALSE))
+    expect_true(test_check_geno("bc", 2, TRUE, FALSE, FALSE, numeric(0), FALSE))
+    expect_true(test_check_geno("bc", 1, FALSE, FALSE, FALSE, numeric(0), FALSE))
+    expect_true(test_check_geno("bc", 2, FALSE, FALSE, FALSE, numeric(0), FALSE))
+    expect_error(test_check_geno("bc", 3, TRUE, FALSE, FALSE, numeric(0), FALSE))
+    expect_error(test_check_geno("bc", 0, FALSE, FALSE, FALSE, numeric(0), FALSE))
+    expect_error(test_check_geno("bc", 3, FALSE, FALSE, FALSE, numeric(0), FALSE))
+
+    # X chromosome female
+    expect_true(test_check_geno("bc", 0, TRUE, TRUE, TRUE, numeric(0), FALSE))
+    expect_true(test_check_geno("bc", 1, TRUE, TRUE, TRUE, numeric(0), FALSE))
+    expect_true(test_check_geno("bc", 2, TRUE, TRUE, TRUE, numeric(0), FALSE))
+    expect_true(test_check_geno("bc", 1, FALSE, TRUE, TRUE, numeric(0), FALSE))
+    expect_true(test_check_geno("bc", 2, FALSE, TRUE, TRUE, numeric(0), FALSE))
+    expect_error(test_check_geno("bc", 3, TRUE, TRUE, TRUE, numeric(0), FALSE))
+    expect_error(test_check_geno("bc", 0, FALSE, TRUE, TRUE, numeric(0), FALSE))
+    expect_error(test_check_geno("bc", 3, FALSE, TRUE, TRUE, numeric(0), FALSE))
+
+    # X chromosome male
+    expect_true(test_check_geno("bc", 0, TRUE, TRUE, FALSE, numeric(0), FALSE))
+    expect_true(test_check_geno("bc", 1, TRUE, TRUE, FALSE, numeric(0), FALSE))
+    expect_true(test_check_geno("bc", 3, TRUE, TRUE, FALSE, numeric(0), FALSE))
+    expect_true(test_check_geno("bc", 1, FALSE, TRUE, FALSE, numeric(0), FALSE))
+    expect_true(test_check_geno("bc", 3, FALSE, TRUE, FALSE, numeric(0), FALSE))
+    expect_error(test_check_geno("bc", 2, TRUE, TRUE, FALSE, numeric(0), FALSE))
+    expect_error(test_check_geno("bc", 4, TRUE, TRUE, FALSE, numeric(0), FALSE))
+    expect_error(test_check_geno("bc", 0, FALSE, TRUE, FALSE, numeric(0), FALSE))
+    expect_error(test_check_geno("bc", 2, FALSE, TRUE, FALSE, numeric(0), FALSE))
+    expect_error(test_check_geno("bc", 4, FALSE, TRUE, FALSE, numeric(0), FALSE))
+
+})
+
+test_that("backcross all_geno works", {
+    expect_equal(test_allgeno("bc", FALSE, FALSE), 1:2)
+    expect_equal(test_allgeno("bc", TRUE, FALSE), 1:3)
+
+    # phase-known shouldn't matter
+    expect_equal(test_allgeno("bc", FALSE, TRUE), 1:2)
+    expect_equal(test_allgeno("bc", TRUE, TRUE), 1:3)
+})
+
+test_that("backcross geno works", {
+
+    # autosome
+    expect_equal(test_geno("bc", FALSE, FALSE, numeric(0), FALSE), 1:2)
+    # X female
+    expect_equal(test_geno("bc", TRUE, TRUE, numeric(0), FALSE), 1:2)
+    # X male
+    expect_equal(test_geno("bc", TRUE, FALSE, numeric(0), FALSE), c(1,3))
+
+    # phase-known shouldn't matter
+    expect_equal(test_geno("bc", FALSE, FALSE, numeric(0), TRUE), 1:2)
+    expect_equal(test_geno("bc", TRUE, TRUE, numeric(0), TRUE), 1:2)
+    expect_equal(test_geno("bc", TRUE, FALSE, numeric(0), TRUE), c(1,3))
+})
+
+test_that("backcross nrec works", {
+
+    # autosome
+    expect_equal(test_nrec("bc", 1, 1, FALSE, FALSE, numeric(0)), 0)
+    expect_equal(test_nrec("bc", 1, 2, FALSE, FALSE, numeric(0)), 1)
+    expect_equal(test_nrec("bc", 2, 1, FALSE, FALSE, numeric(0)), 1)
+    expect_equal(test_nrec("bc", 2, 2, FALSE, FALSE, numeric(0)), 0)
+    # X female
+    expect_equal(test_nrec("bc", 1, 1, TRUE, TRUE, numeric(0)), 0)
+    expect_equal(test_nrec("bc", 1, 2, TRUE, TRUE, numeric(0)), 1)
+    expect_equal(test_nrec("bc", 2, 1, TRUE, TRUE, numeric(0)), 1)
+    expect_equal(test_nrec("bc", 2, 2, TRUE, TRUE, numeric(0)), 0)
+    # X male
+    expect_equal(test_nrec("bc", 1, 1, TRUE, FALSE, numeric(0)), 0)
+    expect_equal(test_nrec("bc", 1, 3, TRUE, FALSE, numeric(0)), 1)
+    expect_equal(test_nrec("bc", 3, 1, TRUE, FALSE, numeric(0)), 1)
+    expect_equal(test_nrec("bc", 3, 3, TRUE, FALSE, numeric(0)), 0)
+
+    # some errors
+    expect_error(test_nrec("bc", 0, 1, FALSE, FALSE, numeric(0)))
+    expect_error(test_nrec("bc", 1, 0, FALSE, FALSE, numeric(0)))
+    expect_error(test_nrec("bc", 3, 1, FALSE, FALSE, numeric(0)))
+    expect_error(test_nrec("bc", 2, 3, FALSE, FALSE, numeric(0)))
+    expect_error(test_nrec("bc", 1, 0, TRUE, TRUE, numeric(0)))
+    expect_error(test_nrec("bc", 0, 2, TRUE, TRUE, numeric(0)))
+    expect_error(test_nrec("bc", 3, 1, TRUE, TRUE, numeric(0)))
+    expect_error(test_nrec("bc", 2, 3, TRUE, TRUE, numeric(0)))
+    expect_error(test_nrec("bc", 1, 0, TRUE, FALSE, numeric(0)))
+    expect_error(test_nrec("bc", 1, 2, TRUE, FALSE, numeric(0)))
+    expect_error(test_nrec("bc", 0, 1, TRUE, FALSE, numeric(0)))
+    expect_error(test_nrec("bc", 2, 2, TRUE, FALSE, numeric(0)))
+
+})
+
+test_that("backcross init works", {
+
+    # autosome
+    expect_equal(test_init("bc", 1, FALSE, FALSE, numeric(0)), log(0.5))
+    expect_equal(test_init("bc", 2, FALSE, FALSE, numeric(0)), log(0.5))
+    # X female
+    expect_equal(test_init("bc", 1, TRUE, TRUE, numeric(0)), log(0.5))
+    expect_equal(test_init("bc", 2, TRUE, TRUE, numeric(0)), log(0.5))
+    # X male
+    expect_equal(test_init("bc", 1, TRUE, FALSE, numeric(0)), log(0.5))
+    expect_equal(test_init("bc", 3, TRUE, FALSE, numeric(0)), log(0.5))
+
+    # phase-known shouldn't matter
+    expect_equal(test_init("bc", 1, FALSE, FALSE, numeric(0), TRUE), log(0.5))
+    expect_equal(test_init("bc", 2, FALSE, FALSE, numeric(0), TRUE), log(0.5))
+    expect_equal(test_init("bc", 1, TRUE, TRUE, numeric(0), TRUE), log(0.5))
+    expect_equal(test_init("bc", 2, TRUE, TRUE, numeric(0), TRUE), log(0.5))
+    expect_equal(test_init("bc", 1, TRUE, FALSE, numeric(0), TRUE), log(0.5))
+    expect_equal(test_init("bc", 3, TRUE, FALSE, numeric(0), TRUE), log(0.5))
+
+    # errors
+    expect_error(test_init("bc", 0, FALSE, FALSE, numeric(0)))
+    expect_error(test_init("bc", 3, FALSE, FALSE, numeric(0)))
+    expect_error(test_init("bc", 0, TRUE, TRUE, numeric(0)))
+    expect_error(test_init("bc", 3, TRUE, TRUE, numeric(0)))
+    expect_error(test_init("bc", 0, TRUE, FALSE, numeric(0)))
+    expect_error(test_init("bc", 2, TRUE, FALSE, numeric(0)))
+    expect_error(test_init("bc", 4, TRUE, FALSE, numeric(0)))
+
+    expect_error(test_init("bc", 0, FALSE, FALSE, numeric(0), TRUE))
+    expect_error(test_init("bc", 3, FALSE, FALSE, numeric(0), TRUE))
+    expect_error(test_init("bc", 0, TRUE, TRUE, numeric(0), TRUE))
+    expect_error(test_init("bc", 3, TRUE, TRUE, numeric(0), TRUE))
+    expect_error(test_init("bc", 0, TRUE, FALSE, numeric(0), TRUE))
+    expect_error(test_init("bc", 2, TRUE, FALSE, numeric(0), TRUE))
+    expect_error(test_init("bc", 4, TRUE, FALSE, numeric(0), TRUE))
+
+})
+
+test_that("backcross emit works", {
+
+    # autosome
+    eps <- 0.01
+    expect_equal(test_emit("bc", 0, 1, eps, FALSE, FALSE, numeric(0)), 0)
+    expect_equal(test_emit("bc", 0, 2, eps, FALSE, FALSE, numeric(0)), 0)
+    expect_equal(test_emit("bc", 1, 1, eps, FALSE, FALSE, numeric(0)), log(1-eps))
+    expect_equal(test_emit("bc", 1, 2, eps, FALSE, FALSE, numeric(0)), log(eps))
+    expect_equal(test_emit("bc", 2, 1, eps, FALSE, FALSE, numeric(0)), log(eps))
+    expect_equal(test_emit("bc", 2, 2, eps, FALSE, FALSE, numeric(0)), log(1-eps))
+    # X female
+    expect_equal(test_emit("bc", 0, 1, eps, TRUE, TRUE, numeric(0)), 0)
+    expect_equal(test_emit("bc", 0, 2, eps, TRUE, TRUE, numeric(0)), 0)
+    expect_equal(test_emit("bc", 1, 1, eps, TRUE, TRUE, numeric(0)), log(1-eps))
+    expect_equal(test_emit("bc", 1, 2, eps, TRUE, TRUE, numeric(0)), log(eps))
+    expect_equal(test_emit("bc", 2, 1, eps, TRUE, TRUE, numeric(0)), log(eps))
+    expect_equal(test_emit("bc", 2, 2, eps, TRUE, TRUE, numeric(0)), log(1-eps))
+    # X male
+    expect_equal(test_emit("bc", 0, 1, eps, TRUE, FALSE, numeric(0)), 0)
+    expect_equal(test_emit("bc", 0, 3, eps, TRUE, FALSE, numeric(0)), 0)
+    expect_equal(test_emit("bc", 1, 1, eps, TRUE, FALSE, numeric(0)), log(1-eps))
+    expect_equal(test_emit("bc", 1, 3, eps, TRUE, FALSE, numeric(0)), log(eps))
+    expect_equal(test_emit("bc", 3, 1, eps, TRUE, FALSE, numeric(0)), log(eps))
+    expect_equal(test_emit("bc", 3, 3, eps, TRUE, FALSE, numeric(0)), log(1-eps))
+
+    # phase-known shouldn't matter
+    eps <- 0.001
+    expect_equal(test_emit("bc", 0, 1, eps, FALSE, FALSE, numeric(0), TRUE), 0)
+    expect_equal(test_emit("bc", 0, 2, eps, FALSE, FALSE, numeric(0), TRUE), 0)
+    expect_equal(test_emit("bc", 1, 1, eps, FALSE, FALSE, numeric(0), TRUE), log(1-eps))
+    expect_equal(test_emit("bc", 1, 2, eps, FALSE, FALSE, numeric(0), TRUE), log(eps))
+    expect_equal(test_emit("bc", 2, 1, eps, FALSE, FALSE, numeric(0), TRUE), log(eps))
+    expect_equal(test_emit("bc", 2, 2, eps, FALSE, FALSE, numeric(0), TRUE), log(1-eps))
+    # X female
+    expect_equal(test_emit("bc", 0, 1, eps, TRUE, TRUE, numeric(0), TRUE), 0)
+    expect_equal(test_emit("bc", 0, 2, eps, TRUE, TRUE, numeric(0), TRUE), 0)
+    expect_equal(test_emit("bc", 1, 1, eps, TRUE, TRUE, numeric(0), TRUE), log(1-eps))
+    expect_equal(test_emit("bc", 1, 2, eps, TRUE, TRUE, numeric(0), TRUE), log(eps))
+    expect_equal(test_emit("bc", 2, 1, eps, TRUE, TRUE, numeric(0), TRUE), log(eps))
+    expect_equal(test_emit("bc", 2, 2, eps, TRUE, TRUE, numeric(0), TRUE), log(1-eps))
+    # X male
+    expect_equal(test_emit("bc", 0, 1, eps, TRUE, FALSE, numeric(0), TRUE), 0)
+    expect_equal(test_emit("bc", 0, 3, eps, TRUE, FALSE, numeric(0), TRUE), 0)
+    expect_equal(test_emit("bc", 1, 1, eps, TRUE, FALSE, numeric(0), TRUE), log(1-eps))
+    expect_equal(test_emit("bc", 1, 3, eps, TRUE, FALSE, numeric(0), TRUE), log(eps))
+    expect_equal(test_emit("bc", 3, 1, eps, TRUE, FALSE, numeric(0), TRUE), log(eps))
+    expect_equal(test_emit("bc", 3, 3, eps, TRUE, FALSE, numeric(0), TRUE), log(1-eps))
+
+    # errors
+    expect_error(test_emit("bc", 0, 0, eps, FALSE, FALSE, numeric(0)))
+    expect_error(test_emit("bc", 0, 3, eps, FALSE, FALSE, numeric(0)))
+    expect_error(test_emit("bc", 3, 1, eps, FALSE, FALSE, numeric(0)))
+    # X female
+    expect_error(test_emit("bc", 0, 0, eps, TRUE, TRUE, numeric(0)))
+    expect_error(test_emit("bc", 0, 3, eps, TRUE, TRUE, numeric(0)))
+    expect_error(test_emit("bc", 3, 1, eps, TRUE, TRUE, numeric(0)))
+    # X male
+    expect_error(test_emit("bc", 0, 0, eps, TRUE, FALSE, numeric(0)))
+    expect_error(test_emit("bc", 0, 2, eps, TRUE, FALSE, numeric(0)))
+    expect_error(test_emit("bc", 2, 1, eps, TRUE, FALSE, numeric(0)))
+
+    # errors
+    expect_error(test_emit("bc", 0, 0, eps, FALSE, FALSE, numeric(0), TRUE))
+    expect_error(test_emit("bc", 0, 3, eps, FALSE, FALSE, numeric(0), TRUE))
+    expect_error(test_emit("bc", 3, 1, eps, FALSE, FALSE, numeric(0), TRUE))
+    # X female
+    expect_error(test_emit("bc", 0, 0, eps, TRUE, TRUE, numeric(0), TRUE))
+    expect_error(test_emit("bc", 0, 3, eps, TRUE, TRUE, numeric(0), TRUE))
+    expect_error(test_emit("bc", 3, 1, eps, TRUE, TRUE, numeric(0), TRUE))
+    # X male
+    expect_error(test_emit("bc", 0, 0, eps, TRUE, FALSE, numeric(0), TRUE))
+    expect_error(test_emit("bc", 0, 2, eps, TRUE, FALSE, numeric(0), TRUE))
+    expect_error(test_emit("bc", 2, 1, eps, TRUE, FALSE, numeric(0), TRUE))
+
+})
+
+test_that("backcross step works", {
+
+    # autosome
+    rf <- 0.01
+    expect_equal(test_step("bc", 1, 1, rf, FALSE, FALSE, numeric(0)), log(1-rf))
+    expect_equal(test_step("bc", 1, 2, rf, FALSE, FALSE, numeric(0)), log(rf))
+    expect_equal(test_step("bc", 2, 1, rf, FALSE, FALSE, numeric(0)), log(rf))
+    expect_equal(test_step("bc", 2, 2, rf, FALSE, FALSE, numeric(0)), log(1-rf))
+    # X female
+    expect_equal(test_step("bc", 1, 1, rf, TRUE, TRUE, numeric(0)), log(1-rf))
+    expect_equal(test_step("bc", 1, 2, rf, TRUE, TRUE, numeric(0)), log(rf))
+    expect_equal(test_step("bc", 2, 1, rf, TRUE, TRUE, numeric(0)), log(rf))
+    expect_equal(test_step("bc", 2, 2, rf, TRUE, TRUE, numeric(0)), log(1-rf))
+    # X male
+    expect_equal(test_step("bc", 1, 1, rf, TRUE, FALSE, numeric(0)), log(1-rf))
+    expect_equal(test_step("bc", 1, 3, rf, TRUE, FALSE, numeric(0)), log(rf))
+    expect_equal(test_step("bc", 3, 1, rf, TRUE, FALSE, numeric(0)), log(rf))
+    expect_equal(test_step("bc", 3, 3, rf, TRUE, FALSE, numeric(0)), log(1-rf))
+
+    # phase-known shouldn't matter
+    rf <- 0.15
+    expect_equal(test_step("bc", 1, 1, rf, FALSE, FALSE, numeric(0), TRUE), log(1-rf))
+    expect_equal(test_step("bc", 1, 2, rf, FALSE, FALSE, numeric(0), TRUE), log(rf))
+    expect_equal(test_step("bc", 2, 1, rf, FALSE, FALSE, numeric(0), TRUE), log(rf))
+    expect_equal(test_step("bc", 2, 2, rf, FALSE, FALSE, numeric(0), TRUE), log(1-rf))
+    # X female
+    expect_equal(test_step("bc", 1, 1, rf, TRUE, TRUE, numeric(0), TRUE), log(1-rf))
+    expect_equal(test_step("bc", 1, 2, rf, TRUE, TRUE, numeric(0), TRUE), log(rf))
+    expect_equal(test_step("bc", 2, 1, rf, TRUE, TRUE, numeric(0), TRUE), log(rf))
+    expect_equal(test_step("bc", 2, 2, rf, TRUE, TRUE, numeric(0), TRUE), log(1-rf))
+    # X male
+    expect_equal(test_step("bc", 1, 1, rf, TRUE, FALSE, numeric(0), TRUE), log(1-rf))
+    expect_equal(test_step("bc", 1, 3, rf, TRUE, FALSE, numeric(0), TRUE), log(rf))
+    expect_equal(test_step("bc", 3, 1, rf, TRUE, FALSE, numeric(0), TRUE), log(rf))
+    expect_equal(test_step("bc", 3, 3, rf, TRUE, FALSE, numeric(0), TRUE), log(1-rf))
+
+    # errors
+    expect_error(test_step("bc", 0, 1, rf, FALSE, FALSE, numeric(0)))
+    expect_error(test_step("bc", 1, 0, rf, FALSE, FALSE, numeric(0)))
+    expect_error(test_step("bc", 3, 1, rf, FALSE, FALSE, numeric(0)))
+    expect_error(test_step("bc", 2, 3, rf, FALSE, FALSE, numeric(0)))
+    # X female
+    expect_error(test_step("bc", 0, 1, rf, TRUE, TRUE, numeric(0)))
+    expect_error(test_step("bc", 1, 0, rf, TRUE, TRUE, numeric(0)))
+    expect_error(test_step("bc", 3, 1, rf, TRUE, TRUE, numeric(0)))
+    expect_error(test_step("bc", 2, 3, rf, TRUE, TRUE, numeric(0)))
+    # X male
+    expect_error(test_step("bc", 0, 1, rf, TRUE, FALSE, numeric(0)))
+    expect_error(test_step("bc", 1, 0, rf, TRUE, FALSE, numeric(0)))
+    expect_error(test_step("bc", 2, 1, rf, TRUE, FALSE, numeric(0)))
+    expect_error(test_step("bc", 3, 2, rf, TRUE, FALSE, numeric(0)))
+
+    # phase-known shouldn't matter
+    expect_error(test_step("bc", 0, 1, rf, FALSE, FALSE, numeric(0), TRUE))
+    expect_error(test_step("bc", 1, 0, rf, FALSE, FALSE, numeric(0), TRUE))
+    expect_error(test_step("bc", 3, 1, rf, FALSE, FALSE, numeric(0), TRUE))
+    expect_error(test_step("bc", 2, 3, rf, FALSE, FALSE, numeric(0), TRUE))
+    # X female
+    expect_error(test_step("bc", 0, 1, rf, TRUE, TRUE, numeric(0), TRUE))
+    expect_error(test_step("bc", 1, 0, rf, TRUE, TRUE, numeric(0), TRUE))
+    expect_error(test_step("bc", 3, 1, rf, TRUE, TRUE, numeric(0), TRUE))
+    expect_error(test_step("bc", 2, 3, rf, TRUE, TRUE, numeric(0), TRUE))
+    # X male
+    expect_error(test_step("bc", 0, 1, rf, TRUE, FALSE, numeric(0), TRUE))
+    expect_error(test_step("bc", 1, 0, rf, TRUE, FALSE, numeric(0), TRUE))
+    expect_error(test_step("bc", 2, 1, rf, TRUE, FALSE, numeric(0), TRUE))
+    expect_error(test_step("bc", 3, 2, rf, TRUE, FALSE, numeric(0), TRUE))
+
+})
