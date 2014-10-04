55b4afc71225e3c77ba39e98305e9ce0ec79cc0e
Karl Broman <kbroman@gmail.com>
2014-10-04 00:22:16 -0500

    debug_util: specify Eigen::

--- src/debug_util.h
+++ src/debug_util.h
@@ -1,9 +1,9 @@
 // print a vector
 void print_vector(const NumericVector x);
 void print_vector(const IntegerVector x);
-void print_vector(const VectorXd x);
-void print_vector(const VectorXi x);
+void print_vector(const Eigen::VectorXd x);
+void print_vector(const Eigen::VectorXi x);
 // print matrix dimension
-void print_matdim(const MatrixXd x);
-void print_matdim(const MatrixXi x);
+void print_matdim(const Eigen::MatrixXd x);
+void print_matdim(const Eigen::MatrixXi x);
 
