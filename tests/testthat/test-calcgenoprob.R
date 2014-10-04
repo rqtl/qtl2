c782590a296abb3d00d1bf71e79ac24ce572591a
Karl Broman <kbroman@gmail.com>
2014-10-03 22:47:18 -0500

    Add test of calc_genoprob in intercross

--- tests/testthat/test-calcgenoprob.R
+++ tests/testthat/test-calcgenoprob.R
@@ -1,7 +1,7 @@
 
 context("calc_genoprob")
 
-test_that("backcross calc_genoprob matches R/qtl", {
+test_that("backcross autosome calc_genoprob matches R/qtl", {
 
     library(qtl)
     data(hyper)
@@ -26,3 +26,30 @@ test_that("backcross calc_genoprob matches R/qtl", {
    }
 
 })
+
+test_that("intercross autosome calc_genoprob matches R/qtl", {
+
+    library(qtl)
+    data(listeria)
+    listeria <- listeria[1:19,]
+    listeria <- calc.genoprob(listeria, step=1, stepwidth="max", err=0.01)
+
+    for(i in 1:19) {
+       g <- listeria$geno[[i]]$data
+       g[is.na(g)] <- 0
+       pr <- listeria$geno[[i]]$prob
+       map <- attr(pr, "map")
+       rf <- mf.h(diff(map))
+       map_index <- match(names(map), colnames(g))-1
+       map_index[is.na(map_index)] <- -1
+
+       pr_qtl2 <- calc_genoprob("f2", t(g), FALSE, rep(FALSE, nind(listeria)),
+                                matrix(ncol=nind(listeria), nrow=0),
+                                rf, map_index, 0.01)
+       pr_qtl2 <- aperm(pr_qtl2, c(2,3,1))
+
+       expect_equivalent(pr, pr_qtl2)
+   }
+
+})
+
