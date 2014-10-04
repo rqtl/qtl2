694cae5caf12fb1f7cbbefcab3441835f50bf58c
Karl Broman <kbroman@gmail.com>
2014-10-03 22:53:37 -0500

    test calc_genoprob for riself/risib (using hyper data)

--- tests/testthat/test-calcgenoprob.R
+++ tests/testthat/test-calcgenoprob.R
@@ -53,3 +53,58 @@ test_that("intercross autosome calc_genoprob matches R/qtl", {
 
 })
 
+
+test_that("risib autosome calc_genoprob matches R/qtl", {
+
+    library(qtl)
+    data(hyper)
+    hyper <- hyper[1:19,]
+    class(hyper)[1] <- "risib"
+    hyper <- calc.genoprob(hyper, step=1, stepwidth="max", err=0.002)
+
+    for(i in 1:19) {
+       g <- hyper$geno[[i]]$data
+       g[is.na(g)] <- 0
+       pr <- hyper$geno[[i]]$prob
+       map <- attr(pr, "map")
+       rf <- mf.h(diff(map))
+       map_index <- match(names(map), colnames(g))-1
+       map_index[is.na(map_index)] <- -1
+
+       pr_qtl2 <- calc_genoprob("risib", t(g), FALSE, rep(FALSE, nind(hyper)),
+                                matrix(ncol=nind(hyper), nrow=0),
+                                rf, map_index, 0.002)
+       pr_qtl2 <- aperm(pr_qtl2, c(2,3,1))
+
+       expect_equivalent(pr, pr_qtl2)
+   }
+
+})
+
+test_that("riself autosome calc_genoprob matches R/qtl", {
+
+    library(qtl)
+    data(hyper)
+    hyper <- hyper[1:19,]
+    class(hyper)[1] <- "riself"
+    hyper <- calc.genoprob(hyper, step=1, stepwidth="max", err=0.002)
+
+    for(i in 1:19) {
+       g <- hyper$geno[[i]]$data
+       g[is.na(g)] <- 0
+       pr <- hyper$geno[[i]]$prob
+       map <- attr(pr, "map")
+       rf <- mf.h(diff(map))
+       map_index <- match(names(map), colnames(g))-1
+       map_index[is.na(map_index)] <- -1
+
+       pr_qtl2 <- calc_genoprob("riself", t(g), FALSE, rep(FALSE, nind(hyper)),
+                                matrix(ncol=nind(hyper), nrow=0),
+                                rf, map_index, 0.002)
+       pr_qtl2 <- aperm(pr_qtl2, c(2,3,1))
+
+       expect_equivalent(pr, pr_qtl2)
+   }
+
+})
+
