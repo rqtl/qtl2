29191982337c91ac3065a55a797836fc526e8545
Karl Broman <kbroman@gmail.com>
2014-10-04 23:22:57 -0500

    Add test of calc_genoprob for f2 X chr with one sex/direction

--- tests/testthat/test-calcgenoprob.R
+++ tests/testthat/test-calcgenoprob.R
@@ -186,3 +186,145 @@ test_that("bc X chr calc_genoprob matches R/qtl", {
 
 })
 
+test_that("f2 X chr all males calc_genoprob matches R/qtl", {
+
+    library(qtl)
+    data(fake.f2)
+    fake.f2 <- fake.f2["X",]
+    fake.f2$pheno$sex <- 1
+    sexpgm <- getsex(fake.f2)
+
+    g <- fake.f2$geno[["X"]]$data
+    g[is.na(g)] <- 0 # missing -> 0
+
+    # males -> 1/3
+    g[g==2] <- 3
+
+    # R/qtl calc.genoprob
+    fake.f2 <- calc.genoprob(fake.f2, step=1, stepwidth="max", error.prob=0.01)
+
+    pr <- fake.f2$geno[["X"]]$prob
+    map <- attr(pr, "map")
+    rf <- mf.h(diff(map))
+    map_index <- match(names(map), colnames(g))-1
+    map_index[is.na(map_index)] <- -1
+
+    pr_qtl2 <- calc_genoprob("f2", t(g), TRUE, (sexpgm$sex==0), rbind(sexpgm$pgm),
+                             rf, map_index, 0.01)
+    pr_qtl2 <- aperm(pr_qtl2, c(2,3,1))
+
+    # het probs all 0
+    expect_true(all(pr_qtl2[,,2] == 0))
+
+    expect_equivalent(pr, pr_qtl2[,,-2])
+
+})
+
+
+test_that("f2 X chr all females calc_genoprob matches R/qtl", {
+
+    library(qtl)
+    data(fake.f2)
+    fake.f2 <- fake.f2["X",]
+    fake.f2$pheno$sex <- 0
+    sexpgm <- getsex(fake.f2)
+
+    g <- fake.f2$geno[["X"]]$data
+    g[is.na(g)] <- 0 # missing -> 0
+
+    # female rev -> 2/3
+    gfemalerev <- g[sexpgm$sex==0 & sexpgm$pgm==1,]
+    gfemalerev[gfemalerev==1] <- 3
+    g[sexpgm$sex==0 & sexpgm$pgm==1,] <- gfemalerev
+
+    # R/qtl calc.genoprob
+    fake.f2 <- calc.genoprob(fake.f2, step=1, stepwidth="max", error.prob=0.01)
+
+    pr <- fake.f2$geno[["X"]]$prob
+    map <- attr(pr, "map")
+    rf <- mf.h(diff(map))
+    map_index <- match(names(map), colnames(g))-1
+    map_index[is.na(map_index)] <- -1
+
+    pr_qtl2 <- calc_genoprob("f2", t(g), TRUE, (sexpgm$sex==0), rbind(sexpgm$pgm),
+                             rf, map_index, 0.01)
+    pr_qtl2 <- aperm(pr_qtl2, c(2,3,1))
+
+    pr_qtl <- reviseXdata("f2", "simple", sexpgm=sexpgm, prob=pr,
+                          cross.attr=attributes(fake.f2))
+
+    expect_equivalent(pr_qtl, pr_qtl2)
+
+})
+
+test_that("f2 X chr all females forw calc_genoprob matches R/qtl", {
+
+    library(qtl)
+    data(fake.f2)
+    fake.f2 <- fake.f2["X",]
+    fake.f2$pheno$sex <- 0
+    fake.f2$pheno$pgm <- 0
+    sexpgm <- getsex(fake.f2)
+
+    g <- fake.f2$geno[["X"]]$data
+    g[is.na(g)] <- 0 # missing -> 0
+
+    # R/qtl calc.genoprob
+    fake.f2 <- calc.genoprob(fake.f2, step=1, stepwidth="max", error.prob=0.01)
+
+    pr <- fake.f2$geno[["X"]]$prob
+    map <- attr(pr, "map")
+    rf <- mf.h(diff(map))
+    map_index <- match(names(map), colnames(g))-1
+    map_index[is.na(map_index)] <- -1
+
+    pr_qtl2 <- calc_genoprob("f2", t(g), TRUE, (sexpgm$sex==0), rbind(sexpgm$pgm),
+                             rf, map_index, 0.01)
+    pr_qtl2 <- aperm(pr_qtl2, c(2,3,1))
+
+    expect_true(all(pr_qtl2[,,3] == 0))
+
+    expect_equivalent(pr, pr_qtl2[,,-3])
+
+})
+
+test_that("f2 X chr all females rev calc_genoprob matches R/qtl", {
+
+    library(qtl)
+    data(fake.f2)
+    fake.f2 <- fake.f2["X",]
+    fake.f2$pheno$sex <- 0
+    fake.f2$pheno$pgm <- 1
+    sexpgm <- getsex(fake.f2)
+
+    g <- fake.f2$geno[["X"]]$data
+    g[is.na(g)] <- 0 # missing -> 0
+
+    # female rev -> 2/3
+    gfemalerev <- g[sexpgm$sex==0 & sexpgm$pgm==1,]
+    gfemalerev[gfemalerev==1] <- 3
+    g[sexpgm$sex==0 & sexpgm$pgm==1,] <- gfemalerev
+
+    # R/qtl calc.genoprob
+    fake.f2 <- calc.genoprob(fake.f2, step=1, stepwidth="max", error.prob=0.01)
+
+    pr <- fake.f2$geno[["X"]]$prob
+    map <- attr(pr, "map")
+    rf <- mf.h(diff(map))
+    map_index <- match(names(map), colnames(g))-1
+    map_index[is.na(map_index)] <- -1
+
+    pr_qtl2 <- calc_genoprob("f2", t(g), TRUE, (sexpgm$sex==0), rbind(sexpgm$pgm),
+                             rf, map_index, 0.01)
+    pr_qtl2 <- aperm(pr_qtl2, c(2,3,1))
+
+    # AA's all 0
+    expect_true(all(pr_qtl2[,,1]==0))
+
+    pr_qtl <- reviseXdata("f2", "simple", sexpgm=sexpgm, prob=pr,
+                          cross.attr=attributes(fake.f2))
+
+    expect_equivalent(pr, pr_qtl2[,,3:2])
+
+})
+
