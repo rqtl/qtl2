39ae8df48ce43b388a08282aa8d76ea6119c45b1
Karl Broman <kbroman@gmail.com>
2014-10-06 01:28:53 -0500

    create pseudomarker map: add minimal version + tests

--- R/create_pseudomarker_map.R
+++ R/create_pseudomarker_map.R
@@ -5,7 +5,7 @@
 # map = vector of marker positions; names = marker names
 # step = distance between pseudomarkers
 # off_end = amount to go beyond the terminal markers
-# tol = tolerance for determining whether a marker
+# tol = tolerance for determining whether a marker hits a pseudomarker
 # pmar_stem = leading part of name of pseudomarkers
 create_pseudomarker_map_grid <-
 function(map, step, off_end=0, tol=0.01, pmar_stem="loc")
@@ -13,6 +13,9 @@ function(map, step, off_end=0, tol=0.01, pmar_stem="loc")
     if(any(is.na(map))) stop("map values can't be missing")
     if(step==0) {
         attr(map, "index") <- seq(along=map)
+        attr(map, "stepwidth") <- "grid"
+        attr(map, "step") <- step
+        attr(map, "off_end") <- off_end
         return(map)
     }
     if(step < 0) stop("step should be >= 0")
@@ -33,6 +36,9 @@ function(map, step, off_end=0, tol=0.01, pmar_stem="loc")
     if(length(pmar) == 0) {
         attr(map, "index") <- seq(along=map)
         attr(map, "grid") <- rep(TRUE, length(map))
+        attr(map, "stepwidth") <- "grid"
+        attr(map, "step") <- step
+        attr(map, "off_end") <- off_end
         return(map)
     }
 
@@ -58,10 +64,93 @@ function(map, step, off_end=0, tol=0.01, pmar_stem="loc")
     # sort the map
     o <- order(map)
     map <- map[o]
+    attr(map, "stepwidth") <- "grid"
     attr(map, "index") <- index[o]
     attr(map, "grid") <- grid[o]
     attr(map, "step") <- step
     attr(map, "off_end") <- off_end
+
+    map
+}
+
+# pseudomarker map, minimal number of pseudomarkers to add
+#
+# map = vector of marker positions; names = marker names
+# step = min distance between markers or pseudomarkers
+# off_end = amount to go beyond the terminal markers
+# tol = tolerance for determining whether a marker hits a pseudomarker
+# pmar_stem = leading part of name of pseudomarkers
+create_pseudomarker_map_min <-
+function(map, step, off_end=0, tol=0.01, pmar_stem="loc")
+{
+    if(any(is.na(map))) stop("map values can't be missing")
+    if(step==0) {
+        attr(map, "index") <- seq(along=map)
+        attr(map, "stepwidth") <- "max"
+        attr(map, "step") <- step
+        attr(map, "off_end") <- off_end
+        return(map)
+    }
+    if(step < 0) stop("step should be >= 0")
+    if(tol >= step) stop("tol should be << step")
+    if(off_end < 0) stop("off_end should be >= 0")
+    if(tol < 0) stop("tol should be >= 0")
+
+    # distance between markers
+    d <- diff(map)
+    if(any(d < 0))
+        stop("marker positions should be sorted")
+
+    pmar <- c(seq(min(map), min(map)-off_end, by=-step)[-1],
+              seq(max(map), max(map)+off_end, by=step)[-1])
+
+    if(length(map) == 1) { # just one marker
+        index <- c(1, rep(0, length(pmar)))
+        map <- c(map, pmar)
+        o <- order(map)
+        map <- map[o]
+        attr(map, "index") <- index[o]
+        attr(map, "stepwidth") <- "max"
+        attr(map, "step") <- step
+        attr(map, "off_end") <- off_end
+        return(map)
+    }
+
+    # intervals needing pseudomarkers (d = distances between adjacent markers
+    int2fill <- which(d > step)
+
+    pmar <- c(pmar, unlist(lapply(int2fill, function(a) {
+        n <- ceiling((map[a+1]-map[a])/step)+1
+        seq(map[a], map[a+1], length.out=n)[-c(1, n)] })))
+
+    if(length(pmar) == 0) {
+        attr(map, "index") <- seq(along=map)
+        attr(map, "stepwidth") <- "max"
+        attr(map, "step") <- step
+        attr(map, "off_end") <- off_end
+        return(map)
+    }
+
+    # no. digits to use in name
+    digits <- ceiling(-log10(step))
+    digits <- ifelse(digits < 0, 0, digits)
+
+    names(pmar) <- paste0(pmar_stem, round(pmar, digits))
+
+    # index of markers vs pseudomarkers
+    index <- c(seq(along=map), rep(0, length(pmar)))
+
+    # add pseudomarkers to markers
+    map <- c(map, pmar)
+
+    # sort the map
+    o <- order(map)
+    map <- map[o]
+    attr(map, "stepwidth") <- "max"
+    attr(map, "step") <- step
+    attr(map, "off_end") <- off_end
+    attr(map, "index") <- index[o]
+    attr(map, "grid") <- grid[o]
 
     map
 }
--- tests/testthat/test-pseudomarkermap.R
+++ tests/testthat/test-pseudomarkermap.R
@@ -29,68 +29,83 @@ test_that("grid-based version works in simple case", {
 
 })
 
+test_that("minimal version works in simple case", {
+
+    #equally-spaced map
+    map <- seq(0, 50, by=2.5)
+
+    # step = marker distance
+    pmap <- create_pseudomarker_map_min(map, step=2.5, off_end=0)
+    expect_equivalent(map, pmap)
+    expect_equal(attr(pmap, "index"), seq(along=map))
+
+    # step = 1
+    pmap <- create_pseudomarker_map_min(map, step=1, off_end=0)
+    expect_equivalent(pmap, seq(0, 50, by=5/6))
+
+    # expected index
+    index <- rep(0, length(pmap))
+    mar <- names(pmap)==""
+    index[mar] <- 1:sum(mar)
+    expect_equal(attr(pmap, "index"), index)
+
+})
+
 # the following are totally not working
 # ...the new routine seems better than the old one
-test_that("grid-based version works in more realistic case", {
+test_that("minimal version works in more realistic case", {
 
     data(hyper)
-
     map <- qtl::pull.map(hyper, chr=1)[[1]]
 
-    pmap <- create_pseudomarker_map_grid(map, step=1.55, off_end=4)
+    pmap <- create_pseudomarker_map_min(map, step=1.55, off_end=4)
 
-    expected <- c(-0.7, 0.85, 2.4, 3.3, 3.95, 5.5, 7.05, 8.6, 10.15,
-                  11.7, 13.25, 14.8, 16.35, 17.9, 19.45, 19.7000000001, 21, 22.55,
-                  24.1, 25.65, 27.2, 28.75, 30.3, 31.85, 32.8000000002, 33.4, 34.95,
-                  35.0000000003, 36.5, 37.2000000004, 38.05, 39.6, 41.15, 41.5000000005,
-                  42.7, 43.7000000006, 43.7000000007, 44.25, 45.8, 47.35, 48.9,
-                  49.2000000008, 50.45, 52, 53.55, 54.6000000009, 55.1, 56.65,
-                  58.2, 59.75, 61.3, 62.85, 64.4, 64.500000001, 65.95, 67.5, 67.8000000011,
-                  69.05, 69.9000000012, 70.6, 72.15, 73.7, 74.3000000013, 75.25,
-                  75.4000000014, 76.8, 78.35, 79.9, 81.45, 82.0000000015, 82.0000000016,
-                  82.0000000017, 82.0000000018, 83, 84.55, 86.1, 86.3000000019,
-                  87.65, 89.2, 90.75, 92.3, 93.85, 94.000000002, 95.4, 96.95, 98.5,
-                  100.05, 101.6, 103.15, 104.7, 106.25, 107.8, 109.35, 110.9, 112.45,
-                  114, 115.55, 115.8000000021, 117.1, 118.65)
-    pmap_names <- c("loc-1", "loc1", "loc2", "D1Mit296", "loc4", "loc6", "loc7", "loc9", "loc10",
-                    "loc12", "loc13", "loc15", "loc16", "loc18", "loc19", "D1Mit123",
-                    "loc21", "loc23", "loc24", "loc26", "loc27", "loc29", "loc30",
-                    "loc32", "D1Mit156", "loc33", "loc35", "D1Mit178", "loc36", "D1Mit19",
-                    "loc38", "loc40", "loc41", "D1Mit7", "loc43", "D1Mit46", "D1Mit132",
-                    "loc44", "loc46", "loc47", "loc49", "D1Mit334", "loc50", "loc52",
-                    "loc54", "D1Mit305", "loc55", "loc57", "loc58", "loc60", "loc61",
-                    "loc63", "loc64", "D1Mit26", "loc66", "loc68", "D1Mit94", "loc69",
-                    "D1Mit218", "loc71", "loc72", "loc74", "D1Mit100", "loc75", "D1Mit102",
-                    "loc77", "loc78", "loc80", "loc81", "D1Mit14", "D1Mit105", "D1Mit159",
-                    "D1Mit267", "loc83", "loc85", "loc86", "D1Mit15", "loc88", "loc89",
-                    "loc91", "loc92", "loc94", "D1Mit456", "loc95", "loc97", "loc98",
-                    "loc100", "loc102", "loc103", "loc105", "loc106", "loc108", "loc109",
-                    "loc111", "loc112", "loc114", "loc116", "D1Mit155", "loc117",
-                    "loc119")
+    expected <- c(0.2, 1.75, 3.3, 4.79090909091818, 6.28181818183636, 7.77272727275454,
+                  9.26363636367273, 10.7545454545909, 12.2454545455091, 13.7363636364273,
+                  15.2272727273455, 16.7181818182636, 18.2090909091818, 19.7000000001,
+                  21.1555555556667, 22.6111111112333, 24.0666666668, 25.5222222223667,
+                  26.9777777779333, 28.4333333335, 29.8888888890667, 31.3444444446333,
+                  32.8000000002, 33.90000000025, 35.0000000003, 36.10000000035,
+                  37.2000000004, 38.6333333337667, 40.0666666671333, 41.5000000005,
+                  42.60000000055, 43.7000000006, 43.7000000007, 45.075000000725,
+                  46.45000000075, 47.825000000775, 49.2000000008, 50.550000000825,
+                  51.90000000085, 53.250000000875, 54.6000000009, 56.0142857152,
+                  57.4285714295, 58.8428571438, 60.2571428581, 61.6714285724, 63.0857142867,
+                  64.500000001, 65.6000000010333, 66.7000000010667, 67.8000000011,
+                  68.85000000115, 69.9000000012, 71.3666666679, 72.8333333346,
+                  74.3000000013, 75.4000000014, 76.72000000142, 78.04000000144,
+                  79.36000000146, 80.68000000148, 82.0000000015, 82.0000000016,
+                  82.0000000017, 82.0000000018, 83.4333333351667, 84.8666666685333,
+                  86.3000000019, 87.84000000192, 89.38000000194, 90.92000000196,
+                  92.46000000198, 94.000000002, 95.45333333534, 96.90666666868,
+                  98.36000000202, 99.81333333536, 101.2666666687, 102.72000000204,
+                  104.17333333538, 105.62666666872, 107.08000000206, 108.5333333354,
+                  109.98666666874, 111.44000000208, 112.89333333542, 114.34666666876,
+                  115.8000000021, 117.3500000021, 118.9000000021)
+    pmap_names <- c("loc0", "loc2", "D1Mit296", "loc5", "loc6", "loc8", "loc9",
+                    "loc11", "loc12", "loc14", "loc15", "loc17", "loc18", "D1Mit123",
+                    "loc21", "loc23", "loc24", "loc26", "loc27", "loc28", "loc30",
+                    "loc31", "D1Mit156", "loc34", "D1Mit178", "loc36", "D1Mit19",
+                    "loc39", "loc40", "D1Mit7", "loc43", "D1Mit46", "D1Mit132", "loc45",
+                    "loc46", "loc48", "D1Mit334", "loc51", "loc52", "loc53", "D1Mit305",
+                    "loc56", "loc57", "loc59", "loc60", "loc62", "loc63", "D1Mit26",
+                    "loc66", "loc67", "D1Mit94", "loc69", "D1Mit218", "loc71", "loc73",
+                    "D1Mit100", "D1Mit102", "loc77", "loc78", "loc79", "loc81", "D1Mit14",
+                    "D1Mit105", "D1Mit159", "D1Mit267", "loc83", "loc85", "D1Mit15",
+                    "loc88", "loc89", "loc91", "loc92", "D1Mit456", "loc95", "loc97",
+                    "loc98", "loc100", "loc101", "loc103", "loc104", "loc106", "loc107",
+                    "loc109", "loc110", "loc111", "loc113", "loc114", "D1Mit155",
+                    "loc117", "loc119")
 
     expect_equivalent(pmap, expected)
     expect_equal(names(pmap), pmap_names)
 
-    expected_index <- c(0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0,
-                        0, 0, 0, 0, 3, 0, 0, 4, 0, 5, 0, 0, 0, 6, 0, 7, 8, 0, 0, 0, 0,
-                        9, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 12, 0, 13, 0,
-                        0, 0, 14, 0, 15, 0, 0, 0, 0, 16, 17, 18, 19, 0, 0, 0, 20, 0,
-                        0, 0, 0, 0, 21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 22,
-                        0, 0)
+    expected_index <- c(0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0,
+                        0, 0, 3, 0, 4, 0, 5, 0, 0, 6, 0, 7, 8, 0, 0, 0, 9, 0, 0, 0, 10,
+                        0, 0, 0, 0, 0, 0, 11, 0, 0, 12, 0, 13, 0, 0, 14, 15, 0, 0, 0,
+                        0, 16, 17, 18, 19, 0, 0, 20, 0, 0, 0, 0, 21, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 22, 0, 0)
     expect_equal(attr(pmap, "index"), expected_index)
-
-    expected_grid <- c(TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
-                       TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE,
-                       TRUE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, FALSE,
-                       TRUE, TRUE, TRUE, FALSE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE,
-                       TRUE, FALSE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE,
-                       TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, FALSE, TRUE,
-                       TRUE, TRUE, FALSE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, FALSE,
-                       FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE,
-                       TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
-                       TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE)
-    expect_equal(attr(pmap, "grid"), expected_grid)
-
 
 })
 
