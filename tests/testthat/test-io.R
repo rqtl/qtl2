fe33ce2866d60885f2e97b2a4b98d2092a3f4e7d
Karl Broman <kbroman@gmail.com>
2014-10-08 08:37:17 -0500

    Revise test-io.R to read local zip files
     - previously was reading sample files from kbroman.org, but was having
       connection problems

--- tests/testthat/test-io.R
+++ tests/testthat/test-io.R
@@ -3,8 +3,8 @@ context("input/output")
 
 test_that("can read grav2 data", {
 
-    yaml_file <- "http://kbroman.org/qtl2/assets/sampledata/grav2/grav2.yaml"
-    grav2 <- read_cross2(yaml_file)
+    zip_file <- system.file("extdata", "grav2.zip", package="qtl2")
+    grav2 <- read_cross2(zip_file)
 
     # attempt to calculate QTL genotype probabilities on chr 1
     pmap <- insert_pseudomarkers(grav2$gmap[[1]], step=1, stepwidth="max", pmar_stem="c1.loc")
@@ -17,8 +17,8 @@ test_that("can read grav2 data", {
 
 test_that("can read iron data", {
 
-    yaml_file <- "http://kbroman.org/qtl2/assets/sampledata/iron/iron.yaml"
-    iron <- read_cross2(yaml_file)
+    zip_file <- system.file("extdata", "iron.zip", package="qtl2")
+    iron <- read_cross2(zip_file)
 
     # attempt to calculate QTL genotype probabilities on X chr
     pmap <- insert_pseudomarkers(iron$gmap[["X"]], step=1, pmar_stem="cX.loc")
