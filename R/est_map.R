a0b37fb7597f0df427f5096ae41fedbb845bfe17
Karl Broman <kbroman@gmail.com>
2014-10-09 16:52:35 -0500

    est_map now takes proper cross2 object

---
+++ R/est_map.R
@@ -0,0 +1,51 @@
+# est_map
+#' Estimate genetic maps
+#'
+#' Uses a hidden Markov model to re-estimate the genetic map for an
+#' experimental cross, with possible allowance for genotyping errors.
+#'
+#' @param cross Object of class \code{"cross2"}. For details, see the
+#' \href{http://kbroman.org/qtl2/assets/vignettes/developer_guide.html}{R/qtl2 developer guide}.
+#' @param error_prob Assumed genotyping error probability
+#' @param map_function Character string indicating the map function to
+#' use to convert genetic distances to recombination fractions.
+#' @param maxit Maximum number of iterations in EM algorithm.
+#' @param tol Tolerance for determining convergence
+#' @param quiet If true, don't print any messages
+est_map <-
+function(cross, error_prob=1e-4,
+         map_function=c("haldane", "kosambi", "c-f", "morgan"),
+         maxit=10000, tol=1e-6, quiet=TRUE)
+{
+    map_function <- match.arg(map_function)
+    if(error_prob < 0) stop("error_prob must be >= 0")
+    if(maxit < 0) stop("maxit must be >= 0")
+    if(tol <= 0) stop("tol must be > 0")
+
+    cross_info <- t(cross$cross_info)
+    is_female <- cross$is_female
+
+    map <- vector("list", length(cross$gmap))
+    names(map) <- names(cross$gmap)
+
+    for(i in seq(along=cross$gmap)) {
+        gmap <- cross$gmap[[i]]
+
+        # omit individuals with < 2 genotypes
+        geno <- cross$geno[[i]]
+        ntyped <- rowSums(geno>0)
+        keep <- (ntyped >= 2)
+
+        rf <- .est_map(cross$crosstype, t(cross$geno[[i]][keep,,drop=FALSE]),
+                       cross$is_x_chr[i], cross$is_female[keep], cross_info[,keep,drop=FALSE],
+                       diff(gmap) %>% mf(map_function), # positions to inter-marker rec frac
+                       error_prob, maxit, tol, !quiet)
+
+        loglik <- attr(rf, "loglik")
+        map[[i]] <- imf(rf, map_function) %>% c(gmap[1], .) %>% cumsum() # rec frac to positions
+        names(map[[i]]) <- names(gmap)
+        attr(map[[i]], "loglik") <- loglik
+    }
+
+    map
+}
