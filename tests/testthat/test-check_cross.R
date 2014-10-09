8c4289bb4bf9cd04a46c873003cefd77433add90
Karl Broman <kbroman@gmail.com>
2014-10-09 00:22:45 -0500

    consolidate test-check_cross and test-check_crosstype

--- tests/testthat/test-check_cross.R
+++ tests/testthat/test-check_cross.R
@@ -1,6 +1,20 @@
 
 context("check cross")
 
+test_that("check_crosstype works appropriately", {
+
+    good_types <- c("bc", "f2", "riself", "risib", "dh", "haploid")
+    for(type in good_types)
+        expect_true(check_crosstype(type))
+
+    expect_error(check_crosstype("f2pk"))
+    expect_false(check_crosstype("f2pk", FALSE))
+
+    expect_error(check_crosstype("bada1gakadsf"))
+    expect_false(check_crosstype("bada1gakadsf", FALSE))
+
+})
+
 test_that("count_invalid_genotypes works appropriately", {
 
     suppressMessages(grav2 <- read_cross2(system.file("extdata", "grav2.zip", package="qtl2")))
