b299014748dd2f368228f4b9efd774406a2738d9
Karl Broman <kbroman@gmail.com>
2014-10-09 00:25:13 -0500

    consolidate check_cross and check_crosstype

--- R/check_cross.R
+++ R/check_cross.R
@@ -1,5 +1,25 @@
 # functions to check the integrity of QTL cross data
 
+# check if a crosstype is supported
+#   if give_error=TRUE, problem results in error
+#   if give_error=FALSE, returns TRUE if supported; FALSE if not
+check_crosstype <-
+function(crosstype, give_error=TRUE)
+{
+    z <- try(.crosstype_supported(crosstype), silent=TRUE)
+
+    if("try-error" %in% class(z) || !z) {
+        if(give_error)
+            stop("Cross type ", crosstype, " not yet supported.")
+        return(invisible(FALSE))
+    }
+
+    invisible(TRUE) # if no error, return TRUE
+}
+
+
+# count the number of invalid genotypes
+# returns a matrix individuals x chromosomes
 count_invalid_genotypes <-
 function(cross)
 {
