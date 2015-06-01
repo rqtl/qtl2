251fccc6c0f41282a131c41be60fb4b76dc24879
Karl Broman <kbroman@gmail.com>
2015-06-01 10:41:20 -0500

    Remove a bunch of stuff related to the geno/scan splot

--- src/linreg_lapack.h
+++
@@ -1,5 +0,0 @@
-// linear regression via LAPACK
-
-// calculate RSS for linear regression via LAPACK
-NumericVector calc_rss_lapack(const NumericMatrix X, const NumericMatrix Y,
-                              const bool skip_dgels, const double tol);
