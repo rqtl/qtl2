461681fbe9048c1fb0fa31251514103eb4fb8839
Karl Broman <kbroman@gmail.com>
2015-06-03 07:06:24 -0500

    Add an r_assert function
     - based on code from Rcpp mailing list, http://bit.ly/rcpp_assert

--- src/r_message.h
+++ src/r_message.h
@@ -5,4 +5,11 @@
 void r_message(std::string text);
 void r_warning(std::string text);
 
+// Following based on code from Luke Miratrix, http://bit.ly/rcpp_assert
+#ifdef NDEBUG
+#define r_assert(EX)
+#else
+#define r_assert(EX) (void)((EX) || (__r_assert (#EX, __FILE__, __LINE__),0))
+#endif
+
 #endif // R_MESSAGE_H
