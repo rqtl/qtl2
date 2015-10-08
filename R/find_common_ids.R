c4142328ac0543ad2d51fc284836acc6fbab3b10
Karl Broman <kbroman@gmail.com>
2015-10-08 09:29:33 -0500

    find_common_ids -> match_ids; new find_common_ids function
    - the find_common_ids function is now match_ids
      (takes two vectors of character strings, returns detailed results
      about what matches what)
    
    - the new find_common_ids function takes an arbitrary number of vectors
      of character strings and returns a vector of IDs that are in *all* of
      the inputs

--- R/find_common_ids.R
+++ R/find_common_ids.R
@@ -1,11 +1,11 @@
-# find common ids
+# match ids
 #
 # takes two vectors of character strings
 # returns a list with
 #   first,second = indexes in both vectors, to make them aligned
 #   firstonly = indexes in first vector that are not in the second
 #   secondonly = indexes in second vector that are not in the first
-find_common_ids <-
+match_ids <-
     function(x,y)
 {
     mxy <- match(x, y)
@@ -26,4 +26,31 @@ find_common_ids <-
         names(result$secondonly) <- y[result$secondonly]
 
     result
+}
+
+# find common ids across a set of vectors of character string
+# return value: ids that are present in *all* of the inputs
+find_common_ids <-
+    function(...)
+{
+    inp <- list(...)
+    n_inp <- length(inp)
+
+    if(n_inp==0)
+        stop("No input")
+
+    if(n_inp==1) {
+        if(length(inp[[1]])==0) return(character(0))
+        return(inp[[1]])
+    }
+
+    reduced <- inp[[1]]
+    for(i in 2:n_inp) {
+        m <- match_ids(reduced, inp[[i]])
+        reduced <- reduced[m$first]
+    }
+
+    if(length(reduced)==0) return(character(0))
+
+    reduced
 }
--- tests/testthat/test-find_common_ids.R
+++ tests/testthat/test-find_common_ids.R
@@ -1,6 +1,6 @@
-context("find_common_ids")
+context("find_common_ids and match_ids")
 
-test_that("find_common_ids works", {
+test_that("match_ids and find_common_ids work", {
 
     set.seed(20151006)
     first <- LETTERS[1:10]
@@ -16,12 +16,14 @@ test_that("find_common_ids works", {
     names(expected$firstonly) <- first[expected$firstonly]
     names(expected$secondonly) <- second[expected$secondonly]
 
-    expect_equal(find_common_ids(first, second), expected)
+    expect_equal(match_ids(first, second), expected)
     expect_equal(first[expected$first], second[expected$second])
+
+    expect_equal(find_common_ids(first, second), first[expected$first])
 
 })
 
-test_that("find_common_ids works with no overlap", {
+test_that("match_ids and find_common_ids work with no overlap", {
 
     set.seed(20151006)
     first <- LETTERS[1:10]
@@ -34,11 +36,14 @@ test_that("find_common_ids works with no overlap", {
     names(expected$firstonly) <- first
     names(expected$secondonly) <- second
 
-    expect_equal(find_common_ids(first, second), expected)
+    expect_equal(match_ids(first, second), expected)
+    expect_equal(first[expected$first], second[expected$second])
+
+    expect_equal(find_common_ids(first, second), first[expected$first])
 
 })
 
-test_that("find_common_ids works with null vectors", {
+test_that("match_ids and find_common_ids work with null vectors", {
 
     set.seed(20151006)
     first <- LETTERS[1:10]
@@ -50,12 +55,16 @@ test_that("find_common_ids works with null vectors", {
                      secondonly=numeric(0))
     names(expected$firstonly) <- first
 
-    expect_equal(find_common_ids(first, second), expected)
+    expect_equal(match_ids(first, second), expected)
+    expect_equal(first[expected$first], second[expected$second])
+    expect_equal(find_common_ids(first, second), first[expected$first])
 
     # switch the role of the two
     expected$secondonly <- expected$firstonly
     expected$firstonly <- numeric(0)
-    expect_equal(find_common_ids(second, first), expected)
+    expect_equal(match_ids(second, first), expected)
+    expect_equal(second[expected$first], first[expected$second])
+    expect_equal(find_common_ids(second, first), second[expected$first])
 
     # same, when second is NULL
     second <- NULL
@@ -66,18 +75,53 @@ test_that("find_common_ids works with null vectors", {
                      secondonly=numeric(0))
     names(expected$firstonly) <- first
 
-    expect_equal(find_common_ids(first, second), expected)
+    expect_equal(match_ids(first, second), expected)
+    expect_equal(find_common_ids(first, second), first[expected$first])
 
     # switch the role of the two (when second is NULL)
     expected$secondonly <- expected$firstonly
     expected$firstonly <- numeric(0)
-    expect_equal(find_common_ids(second, first), expected)
+    expect_equal(match_ids(second, first), expected)
 
     # both NULL
     expected <- list(first=numeric(0),
                      second=numeric(0),
                      firstonly=numeric(0),
                      secondonly=numeric(0))
-    expect_equal(find_common_ids(NULL, NULL), expected)
+    expect_equal(match_ids(NULL, NULL), expected)
+    expect_equal(find_common_ids(NULL, NULL), character(0))
+
+    expect_equal(match_ids(character(0), character(0)), expected)
+    expect_equal(find_common_ids(character(0), character(0)), character(0))
+
+})
+
+
+test_that("find_common_ids with <2 inputs", {
+
+    expect_error(find_common_ids())
+
+    expect_equal(find_common_ids(LETTERS[1:5]), LETTERS[1:5])
+    expect_equal(find_common_ids(NULL), character(0))
+    expect_equal(find_common_ids(character(0)), character(0))
+
+})
+
+
+test_that("find_common_ids with >2 inputs", {
+
+    set.seed(20151008)
+    first <- LETTERS[1:10]
+    second <- sample(LETTERS[3:12])
+    third <- sample(LETTERS[5:14])
+
+    expect_equal(find_common_ids(first, second, third), LETTERS[5:10])
+    expect_equal(sort(find_common_ids(second, first, third)), LETTERS[5:10])
+    expect_equal(sort(find_common_ids(third, second, first)), LETTERS[5:10])
+
+    expect_equal(find_common_ids(third, second, NULL, first), character(0))
+
+    fourth <- sample(c("H", "E", "L", "P"))
+    expect_equal(sort(find_common_ids(third, second, fourth, first)), c("E", "H"))
 
 })
