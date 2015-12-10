a545e41232a15d348163761ae65bf1e1623da752
Karl Broman <kbroman@gmail.com>
2015-12-10 00:42:30 -0600

    Fix bug in calc_kinship ... screwed up array indices

--- src/calc_kinship.cpp
+++ src/calc_kinship.cpp
@@ -13,9 +13,9 @@ NumericMatrix calc_kinship(const NumericVector& prob_array) // array as n_pos x 
     const IntegerVector& dim = prob_array.attr("dim");
     if(dim.size() != 3)
         throw std::invalid_argument("prob_array should be 3-dimensional array of probabilities");
-    const unsigned int n_ind = dim[2];
     const unsigned int n_pos = dim[0];
     const unsigned int n_gen = dim[1];
+    const unsigned int n_ind = dim[2];
     const unsigned int pos_by_gen = n_pos*n_gen;
 
     NumericMatrix result(n_ind, n_ind);
@@ -28,8 +28,8 @@ NumericMatrix calc_kinship(const NumericVector& prob_array) // array as n_pos x 
             double total = 0.0;
             for(unsigned int pos=0; pos<n_pos; pos++) {
                 for(unsigned int gen=0; gen<n_gen; gen++) {
-                    total += prob_array[pos*n_gen + gen + offset_i] *
-                        prob_array[pos*n_gen + gen + offset_j];
+                    total += prob_array[pos + gen*n_pos + offset_i] *
+                        prob_array[pos + gen*n_pos + offset_j];
                 }
             }
             result(ind_i,ind_j) = result(ind_j,ind_i) = total;
