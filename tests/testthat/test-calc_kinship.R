d7b8053ab7aa760826d36e0132ec98a3bd262c71
Karl Broman <kbroman@gmail.com>
2016-02-24 08:55:40 -0600

    Add tests of normalization of calc_kinship

--- tests/testthat/test-calc_kinship.R
+++ tests/testthat/test-calc_kinship.R
@@ -281,3 +281,71 @@ test_that("calc_kinship chr & loco work when multi-core", {
     expect_equal(sim_loco_mc, sim_loco)
 
 })
+
+test_that("calc_kinship normalization works", {
+    iron <- read_cross2(system.file("extdata", "iron.zip", package="qtl2geno"))
+    probs <- calc_genoprob(iron, step=1, error_prob=0.002)
+
+    sim <- calc_kinship(probs)
+    sim_un <- calc_kinship(probs, normalize=FALSE)
+    n <- nrow(sim)
+    sim_n <- sim_un*(n-1)/(n-sum(sim_un)/n)
+    expect_equal(sim_n, sim)
+
+    sim_chr <- calc_kinship(probs, "chr")
+    sim_chr_un <- calc_kinship(probs, "chr", normalize=FALSE)
+    sim_chr_n <- lapply(sim_chr_un,
+                        function(K) {
+                            n <- nrow(K)
+                            K*(n-1)/(n-sum(K)/n)
+                        })
+    expect_equal(sim_chr_n, sim_chr)
+
+    sim_loco <- calc_kinship(probs, "loco")
+    sim_loco_un <- calc_kinship(probs, "loco", normalize=FALSE)
+    sim_loco_n <- lapply(sim_loco_un,
+                        function(K) {
+                            n <- nrow(K)
+                            K*(n-1)/(n-sum(K)/n)
+                        })
+    expect_equal(sim_loco_n, sim_loco)
+
+    sim_loco <- calc_kinship(probs, "loco", omit_x=FALSE)
+    sim_loco_un <- calc_kinship(probs, "loco", omit_x=FALSE, normalize=FALSE)
+    sim_loco_n <- lapply(sim_loco_un,
+                        function(K) {
+                            n <- nrow(K)
+                            K*(n-1)/(n-sum(K)/n)
+                        })
+    expect_equal(sim_loco_n, sim_loco)
+
+
+    ## RIL
+    grav2 <- read_cross2(system.file("extdata", "grav2.zip", package="qtl2geno"))
+    probs <- calc_genoprob(grav2, step=1, error_prob=0.002)
+
+    sim <- calc_kinship(probs)
+    sim_un <- calc_kinship(probs, normalize=FALSE)
+    n <- nrow(sim)
+    sim_n <- sim_un*(n-1)/(n-sum(sim_un)/n)
+    expect_equal(sim_n, sim)
+
+    sim_chr <- calc_kinship(probs, "chr")
+    sim_chr_un <- calc_kinship(probs, "chr", normalize=FALSE)
+    sim_chr_n <- lapply(sim_chr_un,
+                        function(K) {
+                            n <- nrow(K)
+                            K*(n-1)/(n-sum(K)/n)
+                        })
+    expect_equal(sim_chr_n, sim_chr)
+
+    sim_loco <- calc_kinship(probs, "loco")
+    sim_loco_un <- calc_kinship(probs, "loco", normalize=FALSE)
+    sim_loco_n <- lapply(sim_loco_un,
+                        function(K) {
+                            n <- nrow(K)
+                            K*(n-1)/(n-sum(K)/n)
+                        })
+    expect_equal(sim_loco_n, sim_loco)
+
+})
