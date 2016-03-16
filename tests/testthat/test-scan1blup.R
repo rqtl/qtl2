e81a13127b8b45056ceeca6ad79cc1402e4c6e6a
Karl Broman <kbroman@gmail.com>
2016-03-16 10:50:02 -0700

    Clean up test-scan1blup code

--- tests/testthat/test-scan1blup.R
+++ tests/testthat/test-scan1blup.R
@@ -1,5 +1,47 @@
 context("BLUP estimates of QTL effects")
 
+calc_blup <-
+    function(probs, pheno, kinship=NULL, addcovar=NULL, tol=1e-12,
+             quiet=TRUE)
+{
+    addcovar <- cbind(rep(1, length(pheno)), addcovar)
+    if(!is.null(kinship)) {
+        Ke <- decomp_kinship(kinship)
+        Keval <- Ke$values
+        Kevec <- Ke$vectors
+
+        pheno <- Kevec %*% pheno
+        addcovar <- Kevec %*% addcovar
+        probs <- Kevec %*% probs
+
+        lmmfit <- Rcpp_fitLMM(Keval, pheno, addcovar, tol=tol)
+        hsq <- lmmfit$hsq
+        if(!quiet) message("hsq_pg: ", hsq)
+        wts <- 1/sqrt(hsq * Keval + (1-hsq))
+
+        pheno <- wts * pheno
+        addcovar <- wts * addcovar
+        probs <- wts * probs
+    }
+
+    k <- probs %*% t(probs)
+    ke <- decomp_kinship(k)
+    keval <- ke$values
+    kevec <- ke$vectors
+
+    pheno <- kevec %*% pheno
+    addcovar <- kevec %*% addcovar
+
+    lmmfit <- Rcpp_fitLMM(keval, pheno, addcovar, tol=tol)
+    beta <- lmmfit$beta
+    hsq <- lmmfit$hsq
+    if(!quiet) message("hsq_qtl: ", hsq)
+    wts <- hsq * keval + (1-hsq)
+    u <- as.numeric( t(kevec %*% probs) %*% diag(hsq/wts) %*%  (pheno - addcovar %*% beta) )
+    c(u, beta)
+}
+
+
 library(qtl2geno)
 iron <- read_cross2(system.file("extdata", "iron.zip", package="qtl2geno"))
 phe <- iron$pheno[,1,drop=FALSE]
@@ -18,24 +60,9 @@ test_that("scan1blup works with no kinship matrix", {
 
     # brute force BLUPs
     for(i in 1:dim(pr$probs[[1]])[[3]]) {
-        Z <- pr$probs[[1]][,,i]
-        k <- Z %*% t(Z)
-        ke <- decomp_kinship(k)
-        keval <- ke$values
-        kevec <- ke$vectors
-        X <- cbind(rep(1, nrow(phe)))
-        yp <- kevec %*% phe
-        Xp <- kevec %*% X
-        lmmfit <- Rcpp_fitLMM(keval, yp, Xp, tol=1e-12)
-
-        beta <- lmmfit$beta
-        expect_equal(blup_int$coef[i,4], beta, tolerance=1e-6)
-
-        hsq <- lmmfit$hsq
-        wts <- hsq * keval + (1-hsq)
-        u <- as.numeric( t(kevec %*% Z) %*% diag(hsq/wts) %*%  (yp - Xp %*% beta) )
-        names(u) <- colnames(blup$coef)
-        expect_equal(blup_int$coef[i, 1:3], u, tolerance=1e-5)
+        blup_alt <- calc_blup(pr$probs[[1]][,,i], phe)
+        names(blup_alt) <- colnames(blup_int$coef)
+        expect_equal(blup_int$coef[i,], blup_alt, tolerance=1e-5)
     }
 
     # repeat with an additive covariate
@@ -52,25 +79,9 @@ test_that("scan1blup works with no kinship matrix", {
 
     # brute force BLUPs
     for(i in 1:dim(pr$probs[[1]])[[3]]) {
-        Z <- pr$probs[[1]][,,i]
-        k <- Z %*% t(Z)
-        ke <- decomp_kinship(k)
-        keval <- ke$values
-        kevec <- ke$vectors
-        X <- cbind(1, sex)
-        yp <- kevec %*% phe
-        Xp <- kevec %*% X
-        lmmfit <- Rcpp_fitLMM(keval, yp, Xp, tol=1e-12)
-
-        beta <- lmmfit$beta
-        names(beta) <- c("intercept", "ac1")
-        expect_equal(blup_int$coef[i,4:5], beta, tolerance=1e-6)
-
-        hsq <- lmmfit$hsq
-        wts <- hsq * keval + (1-hsq)
-        u <- as.numeric( t(kevec %*% Z) %*% diag(hsq/wts) %*%  (yp - Xp %*% beta) )
-        names(u) <- colnames(blup$coef)[1:3]
-        expect_equal(blup_int$coef[i, 1:3], u, tolerance=1e-6)
+        blup_alt <- calc_blup(pr$probs[[1]][,,i], phe, addcovar=sex)
+        names(blup_alt) <- colnames(blup_int$coef)
+        expect_equal(blup_int$coef[i,], blup_alt, tolerance=1e-5)
     }
 
 })
@@ -94,40 +105,10 @@ test_that("scan1blup works with kinship matrix", {
     expect_equal(blup$coef, blup_int$coef[,c(1:3,5)] + cbind(blup_int$coef[,c(4,4,4)], 0))
 
     # brute force BLUPs
-    Ke <- decomp_kinship(K)
-    Keval <- Ke$values
-    Kevec <- Ke$vectors
-    yp <- Kevec %*% phe
-    X <- cbind(1, sex)
-    Xp <- Kevec %*% X
-    lmmfit <- Rcpp_fitLMM(Keval, yp, Xp, tol=1e-12)
-    hsq <- lmmfit$hsq
-    wts <- 1/sqrt(hsq * Keval + (1-hsq))
-    yp <- wts * yp
-    Xp <- wts * Xp
-    pradj <- pr$probs[[1]]
-    for(i in 1:dim(pradj)[[3]])
-        pradj[,,i] <- Kevec %*% pradj[,,i] * wts
-
-    for(i in 1:dim(pradj)[[3]]) {
-        Z <- pradj[,,i]
-        k <- Z %*% t(Z)
-        ke <- decomp_kinship(k)
-        keval <- ke$values
-        kevec <- ke$vectors
-        ypp <- kevec %*% yp
-        Xpp <- kevec %*% Xp
-        lmmfit <- Rcpp_fitLMM(keval, ypp, Xpp, tol=1e-12)
-
-        beta <- lmmfit$beta
-        names(beta) <- c("intercept", "ac1")
-        expect_equal(blup_int$coef[i,4:5], beta, tolerance=1e-3)
-
-        hsq <- lmmfit$hsq
-        wts <- hsq * keval + (1-hsq)
-        u <- as.numeric( t(kevec %*% Z) %*% diag(hsq/wts) %*%  (ypp - Xpp %*% beta) )
-        names(u) <- colnames(blup$coef)[1:3]
-        expect_equal(blup_int$coef[i, 1:3], u, tolerance=1e-3)
+    for(i in 1:dim(pr$probs[[1]])[[3]]) {
+        blup_alt <- calc_blup(pr$probs[[1]][,,i], phe, K, sex)
+        names(blup_alt) <- colnames(blup_int$coef)
+        expect_equal(blup_int$coef[i,], blup_alt, tolerance=1e-5)
     }
 
 })
@@ -135,8 +116,8 @@ test_that("scan1blup works with kinship matrix", {
 test_that("scan1blup works with kinship matrix on another chromosome", {
 
     pr <- calc_genoprob(iron, step=0)
-    K <- calc_kinship(pr[,c(1:11,13:19,"X")])
-    pr <- pr[,"12"]
+    K <- calc_kinship(pr[,c(1:10,12:19,"X")])
+    pr <- pr[,"11"]
 
     # sex as an additive covariate
     sex <- as.numeric(iron$covar$sex=="m")
@@ -151,40 +132,10 @@ test_that("scan1blup works with kinship matrix on another chromosome", {
     expect_equal(blup$coef, blup_int$coef[,c(1:3,5)] + cbind(blup_int$coef[,c(4,4,4)], 0))
 
     # brute force BLUPs
-    Ke <- decomp_kinship(K)
-    Keval <- Ke$values
-    Kevec <- Ke$vectors
-    yp <- Kevec %*% phe
-    X <- cbind(1, sex)
-    Xp <- Kevec %*% X
-    lmmfit <- Rcpp_fitLMM(Keval, yp, Xp, tol=1e-12)
-    hsq <- lmmfit$hsq
-    wts <- 1/sqrt(hsq * Keval + (1-hsq))
-    yp <- wts * yp
-    Xp <- wts * Xp
-    pradj <- pr$probs[[1]]
-    for(i in 1:dim(pradj)[[3]])
-        pradj[,,i] <- Kevec %*% pradj[,,i] * wts
-
-    for(i in 1:dim(pradj)[[3]]) {
-        Z <- pradj[,,i]
-        k <- Z %*% t(Z)
-        ke <- decomp_kinship(k)
-        keval <- ke$values
-        kevec <- ke$vectors
-        ypp <- kevec %*% yp
-        Xpp <- kevec %*% Xp
-        lmmfit <- Rcpp_fitLMM(keval, ypp, Xpp, tol=1e-12)
-
-        beta <- lmmfit$beta
-        names(beta) <- c("intercept", "ac1")
-        expect_equal(blup_int$coef[i,4:5], beta, tolerance=1e-3)
-
-        hsq <- lmmfit$hsq
-        wts <- hsq * keval + (1-hsq)
-        u <- as.numeric( t(kevec %*% Z) %*% diag(hsq/wts) %*%  (ypp - Xpp %*% beta) )
-        names(u) <- colnames(blup$coef)[1:3]
-        expect_equal(blup_int$coef[i, 1:3], u, tolerance=1e-3)
+    for(i in 1:dim(pr$probs[[1]])[[3]]) {
+        blup_alt <- calc_blup(pr$probs[[1]][,,i], phe, K, sex)
+        names(blup_alt) <- colnames(blup_int$coef)
+        expect_equal(blup_int$coef[i,], blup_alt, tolerance=1e-5)
     }
 
 })
