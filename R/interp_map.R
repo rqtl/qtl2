46ab4bcba1f4ac28ece5b640b850120cd98e507b
Karl Broman <kbroman@gmail.com>
2016-03-17 23:09:50 -0500

    Add example for interp_map()

--- R/interp_map.R
+++ R/interp_map.R
@@ -6,11 +6,26 @@
 #' @param oldmap Map with positions in the original scale, as in \code{map_to_change}.
 #' @param newmap Map with positions in the new scale.
 #'
-#' @export
 #' @return Object of same form as input \code{map} but in the units as in \code{newmap}.
+#'
+#' @examples
+#' # load example data
+#' library(qtl2geno)
+#' iron <- read_cross2(system.file("extdata", "iron.zip", package="qtl2geno"))
+#'
+#' # positions to interpolate from cM to Mbp
+#' tointerp <- list("7" = c(pos7.1= 5, pos7.2=15, pos7.3=25),
+#'                  "9" = c(pos9.1=20, pos9.2=40))
+#'
+#' interp_map(tointerp, iron$gmap, iron$pmap)
+#'
+#' @export
 interp_map <-
     function(map, oldmap, newmap)
 {
+    if(is.null(oldmap)) stop("oldmap is NULL")
+    if(is.null(newmap)) stop("newmap is NULL")
+
     chr <- names(map)
     result <- vector("list", length(map))
     names(result) <- chr
