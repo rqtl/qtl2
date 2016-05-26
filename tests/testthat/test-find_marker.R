ddc268dbedd3c21a719f45e9a41a2df751c531e3
Karl Broman <kbroman@gmail.com>
2016-05-26 11:59:02 -0500

    Add function find_markerpos
    - Find genetic and physical positions of a set of markers within a cross2 object

--- tests/testthat/test-find_marker.R
+++ tests/testthat/test-find_marker.R
@@ -1,4 +1,4 @@
-context("find_marker")
+context("find_marker and find_markerpos")
 
 test_that("find_marker works", {
 
@@ -25,5 +25,46 @@ test_that("find_marker works", {
 
     # error if interval provided and length(chr) != 1
     expect_error(find_marker(iron$gmap, c(18, 19), interval=c(20, 30)))
+
+})
+
+test_that("find_markerpos works", {
+
+    iron <- read_cross2(system.file("extdata", "iron.zip", package="qtl2geno"))
+
+    expected <- data.frame(chr=c("8", "11"),
+                           gmap=c(39.1, 56.9),
+                           pmap=c(82.491410, 112.653628),
+                           stringsAsFactors=FALSE)
+    rownames(expected) <- c("D8Mit294", "D11Mit101")
+    expect_equal(find_markerpos(iron, c("D8Mit294", "D11Mit101")), expected)
+
+    # not-found marker with na.rm=TRUE
+    expect_equal(suppressWarnings(find_markerpos(iron, c("D8Mit294", "blah", "D11Mit101"))), expected)
+    expect_warning(find_markerpos(iron, c("D8Mit294", "blah", "D11Mit101")))
+
+    # not-found marker with na.rm=FALSE
+    expected <- rbind(expected[1,], "blah"=data.frame(chr=NA, gmap=NA, pmap=NA), expected[2,])
+    expect_equal(find_markerpos(iron, c("D8Mit294", "blah", "D11Mit101"), na.rm=FALSE), expected)
+
+    # one marker
+    expect_equal(find_markerpos(iron, "D8Mit294"), expected[1,])
+
+    # no gmap
+    iron_nogmap <- iron
+    iron_nogmap$gmap <- NULL
+    expect_equal(find_markerpos(iron_nogmap, c("D8Mit294", "D11Mit101")), expected[c(1,3),-2])
+    expect_equal(find_markerpos(iron_nogmap, "D11Mit101"), expected[3,-2])
+
+    # no pmap
+    iron_nopmap <- iron
+    iron_nopmap$pmap <- NULL
+    expect_equal(find_markerpos(iron_nopmap, c("D8Mit294", "D11Mit101")), expected[c(1,3),-3])
+    expect_equal(find_markerpos(iron_nopmap, "D11Mit101"), expected[3,-3])
+
+    # no map at all
+    iron_nomap <- iron_nopmap
+    iron_nomap$gmap <- NULL
+    expect_error(find_markerpos(iron_nomap, "D11Mit101"))
 
 })
