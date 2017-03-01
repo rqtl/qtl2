68fd00c8114734c15b6184327fdb5fc9fe1bd30d
Karl Broman <kbroman@gmail.com>
2017-03-01 16:45:54 -0600

    Fix tests of max_scan1 and maxlod

--- tests/testthat/test-max_scan1.R
+++ tests/testthat/test-max_scan1.R
@@ -9,7 +9,8 @@ test_that("max_scan1 works for intercross with two phenotypes", {
     iron <- read_cross2(system.file("extdata", "iron.zip", package="qtl2geno"))
 
     # calculate genotype probabilities
-    probs <- calc_genoprob(iron, step=1, error_prob=0.002)
+    map <- insert_pseudomarkers(iron$gmap, step=1)
+    probs <- calc_genoprob(iron, map, error_prob=0.002)
 
     # grab phenotypes and covariates; ensure that covariates have names attribute
     pheno <- iron$pheno
@@ -23,28 +24,26 @@ test_that("max_scan1 works for intercross with two phenotypes", {
     # maximum of first column
     expected <- data.frame(chr="16",
                            pos=28.6,
-                           liver=max(out$lod[,1]),
+                           liver=max(unclass(out[map,,1])),
                            stringsAsFactors=FALSE)
     rownames(expected) <- "c16.loc29"
-    expect_equal(max(out), expected)
+    expect_equal(max(out,map), expected)
 
     # maximum of spleen column
     expected <- data.frame(chr="9",
                            pos=56.6,
-                           spleen=max(out$lod[,2]),
+                           spleen=max(unclass(out[map,,2])),
                            stringsAsFactors=FALSE)
     rownames(expected) <- "c9.loc57"
-    expect_equal(max(out, lodcolumn="spleen"), expected)
+    expect_equal(max(out, map, lodcolumn="spleen"), expected)
 
     # maximum of first column on chr 2
-    index <- split(1:nrow(out$lod), rep(seq(along=out$map), sapply(out$map, length)))
-    keep <- unlist(index[[2]])
     expected <- data.frame(chr="2",
                            pos=56.8,
-                           liver=max(out$lod[keep,1]),
+                           liver=max(unclass(out[map,"2",1])),
                            stringsAsFactors=FALSE)
     rownames(expected) <- "D2Mit17"
-    expect_equal(max(out, chr="2"), expected)
+    expect_equal(max(out, map, chr="2"), expected)
 
 })
 
@@ -57,7 +56,8 @@ test_that("maxlod works for intercross with two phenotypes", {
     iron <- read_cross2(system.file("extdata", "iron.zip", package="qtl2geno"))
 
     # calculate genotype probabilities
-    probs <- calc_genoprob(iron, step=1, error_prob=0.002)
+    map <- insert_pseudomarkers(iron$gmap, step=1)
+    probs <- calc_genoprob(iron, map, error_prob=0.002)
 
     # grab phenotypes and covariates; ensure that covariates have names attribute
     pheno <- iron$pheno
@@ -69,19 +69,19 @@ test_that("maxlod works for intercross with two phenotypes", {
     out <- scan1(probs, pheno, addcovar=covar, Xcovar=Xcovar)
 
     # overall max
-    expect_equal(maxlod(out), max(out$lod))
+    expect_equal(maxlod(out), max(unclass(out)))
 
-    expect_equal(maxlod(out, c("2", "9")),
-                 max(subset(out, chr=c("2", "9"))$lod))
+    expect_equal(maxlod(out, map, c("2", "9")),
+                 max(unclass(out[map, c("2", "9"), ])))
 
-    expect_equal(maxlod(out, "2"), max(out["2",]$lod, na.rm=TRUE))
-    expect_equal(maxlod(out, "9"), max(out["9",]$lod, na.rm=TRUE))
+    expect_equal(maxlod(out, map, "2"), max(unclass(out[map,"2",]), na.rm=TRUE))
+    expect_equal(maxlod(out, map, "9"), max(unclass(out[map,"9",]), na.rm=TRUE))
 
-    expect_equal( maxlod(out, c("2","8", "11")),
-                 max( maxlod(out, "2"), maxlod(out, "8"), maxlod(out, "11") ) )
+    expect_equal( maxlod(out, map, c("2","8", "11")),
+                 max( maxlod(out, map, "2"), maxlod(out, map, "8"), maxlod(out, map, "11") ) )
 
     # really could be using integers
-    expect_equal( maxlod(out, c(2,8, 11)),
-                 max( maxlod(out, 2), maxlod(out, 8), maxlod(out, 11) ) )
+    expect_equal( maxlod(out, map, c(2,8, 11)),
+                 max( maxlod(out, map, 2), maxlod(out, map, 8), maxlod(out, map, 11) ) )
 
 })
