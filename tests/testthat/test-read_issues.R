ec5119a3199a11a5851ed283011c31cfdb2dbffa
Karl Broman <kbroman@gmail.com>
2017-03-22 16:58:14 -0500

    test-read_issues: test NAs in gmap/pmap

--- tests/testthat/test-read_issues.R
+++ tests/testthat/test-read_issues.R
@@ -10,10 +10,9 @@ test_that("read_cross2 deals with missing marker info", {
     unzipped_files <- utils::unzip(ironfile, exdir=dir)
 
     # drop two markers from the geno file
-    set.seed(79646034)
     geno_file <- file.path(dir, "iron_geno.csv")
     g <- read_csv(geno_file, rownames_included=FALSE)
-    dropped_index <- sort( sample(2:ncol(g), 2) )
+    dropped_index <- c(20,28)
     dropped_markers <- colnames(g)[dropped_index]
     write.table(g[,-dropped_index], file=geno_file, sep=",",
                 row.names=FALSE, col.names=TRUE, quote=FALSE)
@@ -24,31 +23,38 @@ test_that("read_cross2 deals with missing marker info", {
     expect_equal(iron_sub, drop_markers(iron, dropped_markers))
 
     # drop two markers from the gmap file
+    # and give another two missing positions
     gmap_file <- file.path(dir, "iron_gmap.csv")
     gmap <- read_csv(gmap_file, rownames_included=FALSE)
-    dropped_index2 <- sample((1:nrow(gmap))[-(dropped_index-1)], 2)
+    dropped_index2 <- c(14, 51)
     dropped_markers2 <- gmap[dropped_index2,1]
+    na_index2 <- c(5, 56)
+    na_markers2 <- gmap[na_index2,1]
+    gmap[na_index2,3] <- NA
     write.table(gmap[-dropped_index2,], file=gmap_file, sep=",",
                 row.names=FALSE, col.names=TRUE, quote=FALSE)
 
     expect_warning( iron_sub2 <- read_cross2(yaml_file) ) # this gives an error at the moment
 
-    expect_equal(iron_sub2, drop_markers(iron_sub, dropped_markers2))
+    expect_equal(iron_sub2, drop_markers(iron_sub, c(dropped_markers2, na_markers2)))
 
-    dropped_index <- c(dropped_index-1, dropped_index2)
-    dropped_markers <- c(dropped_markers, dropped_markers2)
+    dropped_index <- c(dropped_index-1, dropped_index2, na_index2)
+    dropped_markers <- c(dropped_markers, dropped_markers2, na_markers2)
 
     # drop two markers from the pmap file
+    # and give another one a missing position
     pmap_file <- file.path(dir, "iron_pmap.csv")
     pmap <- read_csv(pmap_file, rownames_included=FALSE)
-    dropped_index3 <- sample((1:nrow(gmap))[-dropped_index], 2)
+    dropped_index3 <- c(4, 29)
     dropped_markers3 <- pmap[dropped_index3,1]
+    na_index3 <- 33
+    na_markers3 <- pmap[na_index3,1]
+    pmap[na_index3,3] <- NA
     write.table(pmap[-dropped_index3,], file=pmap_file, sep=",",
                 row.names=FALSE, col.names=TRUE, quote=FALSE)
 
     expect_warning( iron_sub3 <- read_cross2(yaml_file) ) # this gives an error at the moment
 
-    expect_equal(iron_sub3, drop_markers(iron, c(dropped_markers, dropped_markers3)))
-
+    expect_equal(iron_sub3, drop_markers(iron, c(dropped_markers, dropped_markers3, na_markers3)))
 
 })
