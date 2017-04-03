e4be887a92852ca28631418a1ea31aab5d7bf530
Karl Broman <kbroman@gmail.com>
2017-04-03 11:37:37 -0500

    fit1 tests: small simplifications

--- tests/testthat/test-fit1.R
+++ tests/testthat/test-fit1.R
@@ -1,6 +1,6 @@
 context("fit1")
 
-test_that("fit1 by H-K works", {
+test_that("fit1 by H-K works in intercross", {
 
     library(qtl2geno)
     iron <- read_cross2(system.file("extdata", "iron.zip", package="qtl2geno"))
@@ -14,13 +14,13 @@ test_that("fit1 by H-K works", {
     Xcovar <- get_x_covar(iron)
 
     # calculate LOD scores
-    out <- scan1(probs, iron$pheno, addcovar=covar, Xcovar=Xcovar)
+    out <- scan1(probs, pheno, addcovar=covar, Xcovar=Xcovar)
 
     # estimate coefficients; no covariates for X chromosome
     coef <- lapply(seq_len(length(probs)), function(i) {
         if(i==3) cov <- NULL
         else cov <- covar
-        scan1coef(subset(probs, chr=names(probs)[i]), iron$pheno[,1], addcovar=cov) })
+        scan1coef(subset(probs, chr=names(probs)[i]), pheno, addcovar=cov) })
 
     # fit1, no missing data
     npos <- sapply(probs, function(a) dim(a)[3])
@@ -29,7 +29,7 @@ test_that("fit1 by H-K works", {
                        function(i) {
         if(i==3) { nullcov <- Xcovar; cov <- NULL } # need Xcovar under null on X chr but no other covariates
         else { nullcov <- NULL; cov <- covar }      # sex as covariate; no additional covariates under null
-        fit1(probs[[i]][,,pmar[i]], iron$pheno[,1], addcovar=cov, nullcovar=nullcov) })
+        fit1(probs[[i]][,,pmar[i]], pheno, addcovar=cov, nullcovar=nullcov) })
 
     pos <- cumsum(c(0, npos[-3])) + pmar
     # check LOD vs scan1, plus ind'l contributions to LOD
@@ -46,24 +46,21 @@ test_that("fit1 by H-K works", {
     pheno[c(187, 244)] <- NA
 
     # calculate LOD scores
-    out <- scan1(probs, iron$pheno, addcovar=covar, Xcovar=Xcovar)
+    out <- scan1(probs, pheno, addcovar=covar, Xcovar=Xcovar)
 
     # estimate coefficients; no covariates for X chromosome
     coef <- lapply(seq_len(length(probs)), function(i) {
         if(i==3) cov <- NULL
         else cov <- covar
-        scan1coef(subset(probs, chr=names(probs)[i]), iron$pheno[,1], addcovar=cov) })
+        scan1coef(subset(probs, chr=names(probs)[i]), pheno, addcovar=cov) })
 
     # fit1, no missing data
-    npos <- sapply(probs, function(a) dim(a)[3])
-    pmar <- c(3, 4, 12)
     out_fit1 <- lapply(seq(along=pmar),
                        function(i) {
         if(i==3) { nullcov <- Xcovar; cov <- NULL } # need Xcovar under null on X chr but no other covariates
         else { nullcov <- NULL; cov <- covar }      # sex as covariate; no additional covariates under null
-        fit1(probs[[i]][,,pmar[i]], iron$pheno[,1], addcovar=cov, nullcovar=nullcov) })
+        fit1(probs[[i]][,,pmar[i]], pheno, addcovar=cov, nullcovar=nullcov) })
 
-    pos <- cumsum(c(0, npos[-3])) + pmar
     # check LOD vs scan1, plus ind'l contributions to LOD
     for(i in 1:3) {
         expect_equal(out_fit1[[i]]$lod, out[pos[i],1])
