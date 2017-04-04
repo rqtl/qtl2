318968cdf16d5e3f1fb532aaa89a6be953853d0b
Brian Yandell <byandell@wisc.edu>
2017-04-04 09:09:02 -0500

    if is.null genoprobs, create a 0-col version

--- R/fit1.R
+++ R/fit1.R
@@ -125,6 +125,10 @@ fit1 <-
     }
 
     # genoprobs is a matrix?
+    if(is.null(genoprobs)) {
+      genoprobws <- matrix(nrow = length(pheno), ncol = 0)
+      rownames(geno_null) <- names(pheno)
+    }
     if(!is.matrix(genoprobs))
         stop("genoprobs should be a matrix, individuals x genotypes")
 
