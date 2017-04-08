ed5e23133d105f418944bc9cb19e5a4e63057189
Karl Broman <kbroman@gmail.com>
2017-04-08 16:11:41 -0700

    R/est_map.R: missed a }

--- R/est_map.R
+++ R/est_map.R
@@ -79,6 +79,7 @@ function(cross, error_prob=1e-4,
         unique_cross_group <- unique(sex_crossinfo)
         cross_group <- match(sex_crossinfo, unique_cross_group)-1 # indexes start at 0
         unique_cross_group <- match(seq(along=unique_cross_group), cross_group)-1 # again start at 0
+    }
 
     by_chr_func <- function(chr) {
         # the following avoids a warning in R CMD check
