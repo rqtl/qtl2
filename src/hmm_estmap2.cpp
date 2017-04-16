5b17732013f1874ceeac7e216838480f90228244
Karl Broman <kbroman@gmail.com>
2017-04-16 13:34:27 -0500

    est_map with lowmem=FALSE and quiet=FALSE: print iteration+1 rather than iteration

--- src/hmm_estmap2.cpp
+++ src/hmm_estmap2.cpp
@@ -259,7 +259,7 @@ NumericVector est_map2_grouped(const String crosstype,
 
         if(verbose) {
             double maxdif = max(abs(prev_rec_frac - cur_rec_frac));
-            Rprintf("%4d %.12f\n", it, maxdif);
+            Rprintf("%4d %.12f\n", it+1, maxdif);
         }
 
         // check convergence
