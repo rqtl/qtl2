040d487b63477f7f24302a518fc3a0f1eecf6104
Karl Broman <kbroman@gmail.com>
2017-04-18 14:28:40 -0500

    Add test of reduce_map_gaps()

---
+++ tests/testthat/test-reduce_map_gaps.R
@@ -0,0 +1,22 @@
+context("reduce gaps in a map")
+
+test_that("reduce_map_gaps works", {
+
+    iron <- read_cross2(system.file("extdata", "iron.zip", package="qtl2geno"))
+    map <- iron$gmap
+
+    expect_equal(reduce_map_gaps(map, 1000), map)
+
+    expected <- map
+    expected[[1]][3] <- expected[[1]][2] + 40
+    expected[[4]][2] <- expected[[4]][1] + 40
+    expected[[5]][2] <- expected[[5]][1] + 40
+
+    expect_equal(reduce_map_gaps(map, 40), expected)
+
+    map2 <- map
+    map2[[2]][3:5] <- map2[[2]][3:5]+50
+    expected[[2]][3:5] <- expected[[2]][3:5] - diff(expected[[2]][2:3]) + 40
+    expect_equal(reduce_map_gaps(map2, 40), expected)
+
+})
