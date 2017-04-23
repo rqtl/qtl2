c9364dc71e638f29140f44b79009a805370410fe
Karl Broman <kbroman@gmail.com>
2017-04-23 09:00:05 -0500

    Add test of calc_ll_binreg_eigenchol

---
+++ tests/testthat/test-binreg.R
@@ -0,0 +1,19 @@
+context("logistic regression");
+
+test_that("binreg_eigen functions work", {
+
+    set.seed(94651642)
+
+    n <- 200
+    X <- cbind(1, sample(0:1, n, replace=TRUE), rnorm(n))
+    nu <- X %*% c(0, 0.5, 0.3)
+    p <- exp(nu)/(1+exp(nu))
+    y <- rbinom(n, 1, p)
+
+    out <- glm(y ~ -1 + X, family=binomial(link=logit))
+    pi <- out$fitted
+    expected <- sum(y*log10(pi) + (1-y)*log10(1-pi))
+
+    expect_equal(calc_ll_binreg_eigenchol(X, y, maxit=30), expected)
+
+})
