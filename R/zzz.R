c298d9cf3664369cea172cd95977f4b65b849523
Karl Broman <kbroman@gmail.com>
2017-04-23 23:41:45 -0400

    Avoid "register routines" warnings
     - See <https://github.com/RcppCore/Rcpp/issues/636#issuecomment-280985661>
     - Add `src/R_init.c`, `src/R_init.h`
     - Add `.registration=TRUE` to `useDynLib()` in `NAMESPACE` (in `R/zzz.R`)

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 # the following to get NAMESPACE file
 #' @importFrom Rcpp sourceCpp
-#' @useDynLib qtl2scan
+#' @useDynLib qtl2scan, .registration = TRUE
 NULL
---
+++ src/R_init.h
@@ -0,0 +1,7 @@
+// Register Dynamic Symbols
+#ifndef R_INIT_H
+#define R_INIT_H
+
+void R_init_qtl2scan(DllInfo* info);
+
+#endif // R_INIT_H
