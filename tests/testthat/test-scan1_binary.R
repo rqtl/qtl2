e6b69713ad2f9ed7fe6e1e498afba7b0c8127418
Karl Broman <kbroman@gmail.com>
2017-04-25 06:48:37 -0400

    test-scan1_binary: test the intercross case

--- tests/testthat/test-scan1_binary.R
+++ tests/testthat/test-scan1_binary.R
@@ -45,3 +45,50 @@ test_that("scan1 with binary phenotype gives same result as R/qtl", {
     expect_equal(out1_ic2[,3] , as.numeric(out2_ic2))
 
 })
+
+
+test_that("scan1 with binary phenotype in intercross gives same results as R/qtl", {
+
+    library(qtl)
+    data(listeria)
+    listeria <- listeria[c(4,11,16),] # subset to 3 chromosomes
+    listeria <- calc.genoprob(listeria, error.prob=0.002, step=1)
+    listeria$pheno <- cbind(listeria$pheno, binary=as.numeric(listeria$pheno[,1] == 264))
+    expect_warning(out1 <- scanone(listeria, pheno.col=3, model="binary", method="hk"))
+
+    # out1 map to list
+    map <- split(out1[,2], out1[,1])
+    mapn <- split(rownames(out1), out1[,1])
+    for(i in seq_along(map)) names(map[[i]]) <- mapn[[i]]
+
+    # convert for R/qtl2
+    library(qtl2geno)
+    listeria2 <- convert2cross2(listeria)
+    pr <- calc_genoprob(listeria2, map, error_prob=0.002)
+    out2 <- scan1(pr, listeria2$pheno[,"binary",drop=FALSE], model="binary")
+
+    expect_equal(out1[,3] , as.numeric(out2))
+
+    # add a couple of random covariates
+    X <- matrix(rnorm(nind(listeria)*2, 20, 2), nrow=nind(listeria), ncol=2)
+    rownames(X) <- ind_ids(listeria2)
+
+    expect_warning(out1_ac <- scanone(listeria, pheno.col=3, model="binary", method="hk", addcovar=X))
+    out2_ac <- scan1(pr, listeria2$pheno[,"binary",drop=FALSE], model="binary", addcovar=X)
+    expect_equal(out1_ac[,3] , as.numeric(out2_ac))
+
+    # make one of them an interactive covariate
+    expect_warning(out1_ic <- scanone(listeria, pheno.col=3, model="binary", method="hk",
+                                      addcovar=X, intcovar=X[,1]))
+    out2_ic <- scan1(pr, listeria2$pheno[,"binary",drop=FALSE], model="binary",
+                     addcovar=X, intcovar=X[,1,drop=FALSE])
+    expect_equal(out1_ic[,3] , as.numeric(out2_ic))
+
+    # make both interactive covariates
+    expect_warning(out1_ic2 <- scanone(listeria, pheno.col=3, model="binary", method="hk",
+                                       addcovar=X, intcovar=X))
+    out2_ic2 <- scan1(pr, listeria2$pheno[,"binary",drop=FALSE], model="binary",
+                      addcovar=X, intcovar=X)
+    expect_equal(out1_ic2[,3] , as.numeric(out2_ic2))
+
+})
