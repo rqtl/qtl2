89c9f50247b2aed07e61b6c47746a2322b2285f9
Karl Broman <kbroman@gmail.com>
2017-05-31 22:04:03 -0500

    qtl2_attach(): print messages when loading

--- R/attach.R
+++ R/attach.R
@@ -2,6 +2,9 @@ core <- c("qtl2geno", "qtl2scan", "qtl2plot")
 
 qtl2_attach <- function() {
 
-    lapply(core, library, character.only=TRUE)
+    for(pkg in core) {
+        message("Loading ", pkg)
+        library(pkg, character.only=TRUE)
+    }
 
 }
