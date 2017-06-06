03c45f57c3989ad0b6a2cf5c5331ee9c7fdce137
Karl Broman <kbroman@gmail.com>
2017-06-05 23:50:48 -0500

    message() -> packageStartupMessage() in qtl2_attach()

--- R/attach.R
+++ R/attach.R
@@ -3,7 +3,7 @@ core <- c("qtl2geno", "qtl2scan", "qtl2plot")
 qtl2_attach <- function() {
 
     for(pkg in core) {
-        message("Loading ", pkg)
+        packageStartupMessage("Loading ", pkg)
         library(pkg, character.only=TRUE)
     }
 
