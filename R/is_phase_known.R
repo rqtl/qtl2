a1a09fcd2ae210f43882f9d4ceb40833b7768e9a
Karl Broman <kbroman@gmail.com>
2017-07-31 13:06:11 -0500

    Don't export is_phase_known()

--- R/is_phase_known.R
+++ R/is_phase_known.R
@@ -1,19 +1,4 @@
-#' Is cross phase-known
-#'
-#' Determine if a cross is of phase-known type
-#'
-#' @param cross Object of class \code{"cross2"}. For details, see the
-#' \href{http://kbroman.org/qtl2/assets/vignettes/developer_guide.html}{R/qtl2 developer guide}.
-#'
-#' @return TRUE/FALSE indicating whether the cross is phase-known or not.
-#'
-#' @export
-#' @keywords utilities
-#'
-#' @examples
-#' grav2 <- read_cross2(system.file("extdata", "grav2.zip", package="qtl2geno"))
-#' is_phase_known(grav2)
-
+# Determine if a cross is of phase-known type
 is_phase_known <-
     function(cross)
 {
