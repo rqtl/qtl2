3731c5ec22ccaa9888382b3a0ecab87e920b0c3c
Karl Broman <kbroman@gmail.com>
2017-08-03 00:46:41 -0500

    Fix bug in inst/scripts/create_ccvariants_small.R

--- inst/scripts/create_ccvariants_small.R
+++ inst/scripts/create_ccvariants_small.R
@@ -11,7 +11,7 @@ description$description <- paste(description$description, "(subset to 2 regions)
 
 tab <- dbGetQuery(db, paste("SELECT * FROM variants WHERE",
                             "(chr=='2' AND pos >= 96500000 AND pos <= 98500000) OR",
-                            "(chr=='3' AND pos >= 14000000 AND pos <= 16000000)")
+                            "(chr=='3' AND pos >= 14000000 AND pos <= 16000000)"))
 dbDisconnect(db)
 
 # write to new database
@@ -19,7 +19,7 @@ dbfile <- "../extdata/cc_variants_small.sqlite"
 if(file.exists(dbfile)) unlink(dbfile)
 db <- dbConnect(SQLite(), dbfile)
 dbWriteTable(db, "variants", tab)
-dbGetQuery(db, "CREATE INDEX chr_pos ON genes (chr, pos)")
+dbGetQuery(db, "CREATE INDEX chr_pos ON variants (chr, pos)")
 
 # add description table
 dbWriteTable(db, "description", description, append=TRUE)
