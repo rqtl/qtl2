bdec763d131eb679a2d16ed84fb97272423a2834
Karl Broman <kbroman@gmail.com>
2017-08-03 12:31:23 -0500

    create_ccvariants.R: need to change strain names for SVs to match SNPs/indels

--- inst/scripts/create_ccvariants.R
+++ inst/scripts/create_ccvariants.R
@@ -384,8 +384,11 @@ svs <- data.frame(snp_id=paste0("SV_", svs[,"#CHROM"], "_", svs[,"START"], "_", 
                   sdp=qtl2scan::calc_sdp(gbin),
                   ensembl_gene=NA,
                   consequence=NA,
+                  gnum,
                   type="SV",
                   stringsAsFactors=FALSE)
+# fix column names to match what we got in the other variant types
+colnames(svs)[8:15] <- sub("_", ".", colnames(svs)[8:15], fixed=TRUE)
 
 dbWriteTable(db, "variants", svs, row.names=FALSE, overwrite=FALSE,
              append=TRUE, field.types=NULL)
