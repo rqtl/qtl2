eaba3b54e1d6d7479669df2a3c6256fa4ddf1415
Karl Broman <kbroman@gmail.com>
2017-08-04 14:56:34 -0500

    Update inst/extdata/cc_variants_small.sqlite

--- tests/testthat/test-create_variant_query_func.R
+++ tests/testthat/test-create_variant_query_func.R
@@ -11,8 +11,18 @@ test_that("create_variant_query_func works", {
                                alleles = c("A|T", "T|C", "G|T"), sdp = c(32L, 64L, 64L),
                                ensembl_gene = c("ENSMUSG00000050587", "ENSMUSG00000050587", "ENSMUSG00000050587"),
                                consequence = c("intron_variant", "intron_variant", "intron_variant"),
+                               A_J = c(1L, 1L, 1L),
+                               C57BL_6J = c(1L, 1L, 1L),
+                               "129S1_SvImJ" = c(1L, 1L, 1L),
+                               NOD_ShiLtJ = c(1L, 1L, 1L),
+                               NZO_HlLtJ = c(1L, 1L, 1L),
+                               CAST_EiJ = c(2L, 1L, 1L),
+                               PWK_PhJ = c(1L, 2L, 2L),
+                               WSB_EiJ = c(1L, 1L, 1L),
                                type = c("snp", "snp", "snp")),
-                          .Names = c("snp_id", "chr", "pos", "alleles", "sdp", "ensembl_gene", "consequence", "type"),
+                          .Names = c("snp_id", "chr", "pos", "alleles", "sdp", "ensembl_gene", "consequence",
+                                     "A_J", "C57BL_6J", "129S1_SvImJ", "NOD_ShiLtJ", "NZO_HlLtJ", "CAST_EiJ", "PWK_PhJ", "WSB_EiJ",
+                                     "type"),
                           row.names = c(NA, -3L), class = "data.frame")
 
     expect_equal(qf(2, 97.3, 97.3002), expected)
