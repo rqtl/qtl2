6af001ed4fe6b17180b9f3b393279dfe65dd940e
Karl Broman <kbroman@gmail.com>
2017-09-15 11:48:34 -0500

    zzz.R -> qtl2scan-package; add qtl2scan-package doc with links to vignettes

---
+++ R/qtl2scan-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib qtl2scan, .registration=TRUE
+#'
+#' @section Vignettes:
+#' \itemize{
+#' \item \href{http://kbroman.org/qtl2/assets/vignettes/user_guide.html}{user guide}
+#' \item \href{http://kbroman.org/qtl2/assets/vignettes/input_files.html}{input file formats}
+#' \item \href{http://kbroman.org/qtl2/pages/prep_do_data.html}{preparing DO mouse data for R/qtl2}
+#' \item \href{http://kbroman.org/qtl2/assets/vignettes/rqtl_diff.html}{differences between R/qtl and R/qtl2}
+#' \item \href{http://kbroman.org/qtl2/assets/vignettes/developer_guide.html}{developer guide}
+#' }
+"_PACKAGE"
--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-# the following to get NAMESPACE file
-#' @importFrom Rcpp sourceCpp
-#' @useDynLib qtl2scan, .registration=TRUE
-NULL
