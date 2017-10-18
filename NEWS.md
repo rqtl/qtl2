ae0bffc9a7c8b0d3ecdd26820067cbd47679f75d
Karl Broman <kbroman@gmail.com>
2017-10-18 13:49:31 -0500

    Add function qtl2version()

---
+++ NEWS.md
@@ -0,0 +1,12 @@
+## qtl2 0.5-6 (2017-10-18)
+
+### New features
+
+- Added `qtl2version()`
+
+
+## qtl2 0.5-3 (2017-09-14)
+
+### New features
+
+- Have the package import [qtl2db](https://github.com/rqtl/qtl2db)
---
+++ R/qtl2version.R
@@ -0,0 +1,33 @@
+#' List R/qtl2 package versions.
+#'
+#' List versions of all installed R/qtl2 packages.
+#'
+#' @return Data frame with two columns: package name and version.
+#'
+#' @examples
+#' qtl2version()
+#'
+#' @export
+
+qtl2version <-
+    function()
+{
+    # grab all installed packages
+    z <- installed.packages()
+
+    # pull out those whose names start with "qtl2"
+    qtl2 <- grep("^qtl2", z[,"Package"])
+
+    # pull out the key columns, as a data frame
+    result <- as.data.frame(z[qtl2,c("Package", "Version"),drop=FALSE],
+                            stringsAsFactors=FALSE)
+
+    # reorder to put core packages first
+    core <- c("qtl2", "qtl2convert", "qtl2db", "qtl2geno", "qtl2plot", "qtl2scan", "qtl2bioc")
+    result <- result[order(result$Package %in% core, decreasing=TRUE),,drop=FALSE]
+
+    # make the rownames numbers
+    rownames(result) <- 1:nrow(result)
+
+    result
+}
