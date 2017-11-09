a8aab712fdedaf157a0a7c5bc5b798fd27015599
Karl Broman <kbroman@gmail.com>
2017-11-09 07:16:06 -0600

    docs to markdown

--- R/qtl2-package.R
+++ R/qtl2-package.R
@@ -7,12 +7,12 @@
 #' @importFrom qtl2convert probs_doqtl_to_qtl2
 #' @importFrom qtl2db create_gene_query_func
 #'
+#' @md
+#'
 #' @section Vignettes:
-#' \itemize{
-#' \item \href{http://kbroman.org/qtl2/assets/vignettes/user_guide.html}{user guide}
-#' \item \href{http://kbroman.org/qtl2/assets/vignettes/input_files.html}{input file formats}
-#' \item \href{http://kbroman.org/qtl2/pages/prep_do_data.html}{preparing DO mouse data for R/qtl2}
-#' \item \href{http://kbroman.org/qtl2/assets/vignettes/rqtl_diff.html}{differences between R/qtl and R/qtl2}
-#' \item \href{http://kbroman.org/qtl2/assets/vignettes/developer_guide.html}{developer guide}
-#' }
+#' * [user guide](http://kbroman.org/qtl2/assets/vignettes/user_guide.html)
+#' * [input file formats](http://kbroman.org/qtl2/assets/vignettes/input_files.html)
+#' * [preparing DO mouse data for R/qtl2](http://kbroman.org/qtl2/pages/prep_do_data.html)
+#' * [differences between R/qtl and R/qtl2](http://kbroman.org/qtl2/assets/vignettes/rqtl_diff.html)
+#' * [developer guide](http://kbroman.org/qtl2/assets/vignettes/developer_guide.html)
 "_PACKAGE"
--- R/qtl2version.R
+++ R/qtl2version.R
@@ -1,6 +1,9 @@
 #' List R/qtl2 package versions.
 #'
-#' List versions of all installed R/qtl2 packages.
+#' List versions of all installed R/qtl2 packages, as shown in the
+#' results of [utils::installed.packages()].
+#'
+#' @md
 #'
 #' @return Data frame with two columns: package name and version.
 #'
