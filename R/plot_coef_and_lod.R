9ee55687fffce9dfbb89d0410e001b8ff3f35ccf
Karl Broman <kbroman@gmail.com>
2017-11-21 21:36:44 -0600

    plot_coef_and_lod() failed to pass bgcolor to plot_scan1()
     - Fixes Issue #42.

--- R/plot_coef_and_lod.R
+++ R/plot_coef_and_lod.R
@@ -53,6 +53,7 @@ plot_coef_and_lod <-
 
     par(mar=bottom_mar)
     plot_scan1(scan1_output, map, lodcolumn=1, col=col_lod, ylab=ylab_lod,
-               add=FALSE, gap=gap, vlines=vlines, vlines_col=vlines_col,
+               add=FALSE, gap=gap, bgcolor=bgcolor, altbgcolor=altbgcolor,
+               vlines=vlines, vlines_col=vlines_col,
                vlines_lwd=vlines_lwd, vlines_lty=vlines_lty, ...)
 }
