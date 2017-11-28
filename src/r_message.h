b794c77895c05c1e1e2db2e8d99f0bc9ba17f24e
Karl Broman <kbroman@gmail.com>
2017-11-28 01:52:54 -0600

    Remove git conflict messages from src/random.h

--- src/r_message.h
+++ src/r_message.h
@@ -2,20 +2,15 @@
 #ifndef R_MESSAGE_H
 #define R_MESSAGE_H
 
-<<<<<<< HEAD
 #include <string>
-=======
 #include <Rcpp.h>
 #include <R_ext/Error.h>
->>>>>>> qtl2scan/master
 
 #define RQTL2_NODEBUG 1 // ignore debugging code
 
 void r_message(std::string text);
 void r_warning(std::string text);
 
-<<<<<<< HEAD
-=======
 // Following based on code from Luke Miratrix, http://bit.ly/rcpp_assert
 #ifdef RQTL2_NODEBUG
 #define r_assert(EX)
@@ -25,6 +20,5 @@ void r_warning(std::string text);
 
 // r_enforce is like r_assert but always works
 #define r_enforce(EX) (void)((EX) || (__r_assert (#EX, __FILE__, __LINE__),0))
->>>>>>> qtl2scan/master
 
 #endif // R_MESSAGE_H
