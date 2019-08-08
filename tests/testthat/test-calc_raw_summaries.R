a810544b273136f6698198e91a1cad96dc541c39
Karl Broman <kbroman@gmail.com>
2019-08-08 10:23:32 -0500

    Add tests of calc_raw_het by marker and calc_raw_geno_freq with multicore

--- tests/testthat/test-calc_raw_summaries.R
+++ tests/testthat/test-calc_raw_summaries.R
@@ -23,6 +23,12 @@ test_that("calculation of raw summaries work", {
     expect_true(all(het >= 0 & het <= 1))
     expect_equal(het, rowMeans(g==2, na.rm=TRUE))
 
+    # calc_raw_maf() by marker
+    het <- calc_raw_het(DOex, "marker")
+    expect_equal(names(het), mnames)
+    expect_true(all(het >= 0 & het <= 1))
+    expect_equal(het, colMeans(g==2, na.rm=TRUE))
+
     # calc_raw_maf()
     maf_ind <- calc_raw_maf(DOex)
     maf_mar <- calc_raw_maf(DOex, "marker")
@@ -46,5 +52,11 @@ test_that("calculation of raw summaries work", {
     expect_equal(dimnames(gf_mar), list(mnames, c("AA", "AB", "BB")))
     expect_equal(gf_ind, cbind(AA=rowMeans(g==1, na.rm=TRUE), AB=rowMeans(g==2, na.rm=TRUE), BB=rowMeans(g==3, na.rm=TRUE)))
     expect_equal(gf_mar, cbind(AA=colMeans(g==1, na.rm=TRUE), AB=colMeans(g==2, na.rm=TRUE), BB=colMeans(g==3, na.rm=TRUE)))
+
+    # multi-core
+    gf_ind_2 <- calc_raw_geno_freq(DOex, cores=8)
+    gf_mar_2 <- calc_raw_geno_freq(DOex, "marker", cores=8)
+    expect_equal(gf_ind_2, gf_ind)
+    expect_equal(gf_mar_2, gf_mar)
 
 })
