54b07594b7b4f9027181d46d198a48b532e3e566
Karl Broman <kbroman@gmail.com>
2021-07-12 13:31:21 -0500

    Add a test of create_snpinfo() from Issue #195

--- tests/testthat/test-create_snpinfo.R
+++ tests/testthat/test-create_snpinfo.R
@@ -21,3 +21,23 @@ test_that("create_snpinfo works", {
     expect_equivalent(snpinfo$sdp, calc_sdp(t(fg[,colSums(fg==0)==0])))
 
 })
+
+
+test_that("create_snpinfo works for arab data", {
+
+    skip_if(isnt_karl(), "this test only run locally")
+
+    file <- paste0("https://raw.githubusercontent.com/rqtl/",
+               "qtl2data/master/ArabMAGIC/arabmagic_tair9.zip")
+    arab <- read_cross2(file)
+
+    snpinfo <- create_snpinfo(arab)
+
+    # expected markers, dropping non-informative or with missing founder genotype
+    tmp <- drop_nullmarkers(arab)
+    fg <- do.call("cbind", tmp$founder_geno)
+    fg <- fg[,colSums(is.na(fg) | fg==0) == 0]
+
+    expect_equal(nrow(snpinfo), ncol(fg))
+    expect_equal(rownames(snpinfo), colnames(fg))
+})
