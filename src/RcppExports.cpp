b6760db4d1c2e203cf46d8d997af1e3e8dd35f62
Karl Broman <kbroman@gmail.com>
2021-07-12 14:27:35 -0500

    Update to RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // arrange_genes
 IntegerVector arrange_genes(const NumericVector& start, const NumericVector& end);
 RcppExport SEXP _qtl2_arrange_genes(SEXP startSEXP, SEXP endSEXP) {
