76bf9fdd7b07f9a2bd2e4cb289fa069654c96472
Karl Broman <kbroman@gmail.com>
2021-10-11 08:56:33 -0500

    Add to list of documentation in man/qtl2-package.Rd

--- R/qtl2-package.R
+++ R/qtl2-package.R
@@ -4,8 +4,14 @@
 #'
 #' @section Vignettes:
 #' * [user guide](https://kbroman.org/qtl2/assets/vignettes/user_guide.html)
+#' * [categorized list of functions in R/qtl2](pages/rqtl2_functions.html)
 #' * [input file formats](https://kbroman.org/qtl2/assets/vignettes/input_files.html)
+#' * [using qtl2fst for on-disk genotype probabilities](assets/vignettes/qtl2fst.html)
 #' * [preparing DO mouse data for R/qtl2](https://kbroman.org/qtl2/pages/prep_do_data.html)
+#' * [genotype diagnostics for diversity outbred mice](assets/vignettes/do_diagnostics.html)
+#' * [identifying sample mix-ups in diversity outbred mice](assets/vignettes/do_mixups.html)
 #' * [differences between R/qtl and R/qtl2](https://kbroman.org/qtl2/assets/vignettes/rqtl_diff.html)
 #' * [developer guide](https://kbroman.org/qtl2/assets/vignettes/developer_guide.html)
+#' * [Tutorial on R/qtl2](https://smcclatchy.github.io/mapping/) by
+#'   [Susan McClatchy](https://github.com/smcclatchy) and [Dan Gatti](https://github.com/dmgatti)
 "_PACKAGE"
